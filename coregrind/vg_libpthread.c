03b823c2a672649f29ab628eee27f93c15ad41f3
Julian Seward <jseward@acm.org>
2002-04-20 13:59:33 +0000

    Add pthread_condattr_destroy.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@104

--- coregrind/vg_libpthread.c
+++ coregrind/vg_libpthread.c
@@ -308,6 +308,10 @@ int pthread_condattr_init(pthread_condattr_t *attr)
    return 0;
 }
 
+int pthread_condattr_destroy(pthread_condattr_t *attr)
+{
+   return 0;
+}
 
 int pthread_cond_init( pthread_cond_t *cond,
                        const pthread_condattr_t *cond_attr)

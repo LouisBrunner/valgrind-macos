84ec9e3d9d0424621f4f371483051a173563fc32
Julian Seward <jseward@acm.org>
2002-09-30 23:17:49 +0000

    Give weak global binding for fns which are to be superposed.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@1146

--- coregrind/vg_default.c
+++ coregrind/vg_default.c
@@ -69,21 +69,25 @@ void non_fund_panic ( Char* fn )
    Fundamental template functions
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 void SK_(pre_clo_init)(VgNeeds* needs, VgTrackEvents* track)
 {
    fund_panic("SK_(pre_clo_init)");
 }
 
+__attribute__ ((weak))
 void SK_(post_clo_init)(void)
 {
    fund_panic("SK_(post_clo_init)");
 }
 
+__attribute__ ((weak))
 UCodeBlock* SK_(instrument)(UCodeBlock* cb, Addr not_used)
 {
    fund_panic("SK_(instrument)");
 }
 
+__attribute__ ((weak))
 void SK_(fini)(void)
 {
    fund_panic("SK_(fini)");
@@ -93,32 +97,38 @@ void SK_(fini)(void)
    For error reporting and suppression handling
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 Bool SK_(eq_SkinError)(VgRes res, SkinError* e1, SkinError* e2)
 {
    non_fund_panic("SK_(eq_SkinError)");
 }
 
+__attribute__ ((weak))
 void SK_(pp_SkinError)(SkinError* ec, void (*pp_ExeContext)(void))
 {
    non_fund_panic("SK_(pp_SkinError)");
 }
 
+__attribute__ ((weak))
 void SK_(dup_extra_and_update)(SkinError* ec)
 {
    non_fund_panic("SK_(dup_extra_and_update)");
 }
 
+__attribute__ ((weak))
 Bool SK_(recognised_suppression)(Char* name, SuppKind* skind)
 {
    non_fund_panic("SK_(recognised_suppression)");
 }
 
+__attribute__ ((weak))
 Bool SK_(read_extra_suppression_info)(Int fd, Char* buf, 
                                        Int nBuf, SkinSupp *s)
 {
    non_fund_panic("SK_(read_extra_suppression_info)");
 }
 
+__attribute__ ((weak))
 Bool SK_(error_matches_suppression)(SkinError* ec, SkinSupp* su)
 {
    non_fund_panic("SK_(error_matches_suppression)");
@@ -129,6 +139,7 @@ Bool SK_(error_matches_suppression)(SkinError* ec, SkinSupp* su)
    For throwing out basic block level info when code is invalidated
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 void SK_(discard_basic_block_info)(Addr a, UInt size)
 {
    non_fund_panic("SK_(discard_basic_block_info)");
@@ -139,6 +150,7 @@ void SK_(discard_basic_block_info)(Addr a, UInt size)
    For throwing out basic block level info when code is invalidated
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 void SK_(written_shadow_regs_values)(UInt* gen_reg, UInt* eflags)
 {
    non_fund_panic("SK_(written_shadow_regs_values)");
@@ -149,11 +161,13 @@ void SK_(written_shadow_regs_values)(UInt* gen_reg, UInt* eflags)
    Command line arg template function
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 Bool SK_(process_cmd_line_option)(Char* argv)
 {
    non_fund_panic("SK_(process_cmd_line_option)");
 }
 
+__attribute__ ((weak))
 Char* SK_(usage)(void)
 {
    non_fund_panic("SK_(usage)");
@@ -163,6 +177,7 @@ Char* SK_(usage)(void)
    Client request template function
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 UInt SK_(handle_client_request)(ThreadState* tst, UInt* arg_block)
 {
    non_fund_panic("SK_(handle_client_request)");
@@ -172,26 +187,31 @@ UInt SK_(handle_client_request)(ThreadState* tst, UInt* arg_block)
    UCode extension
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 void SK_(emit_XUInstr)(UInstr* u, RRegSet regs_live_before)
 {
    non_fund_panic("SK_(emit_XUInstr)");
 }
 
+__attribute__ ((weak))
 Bool SK_(sane_XUInstr)(Bool beforeRA, Bool beforeLiveness, UInstr* u)
 {
    non_fund_panic("SK_(sane_XUInstr)");
 }
 
+__attribute__ ((weak))
 Char* SK_(name_XUOpcode)(Opcode opc)
 {
    non_fund_panic("SK_(name_XUOpcode)");
 }
 
+__attribute__ ((weak))
 void SK_(pp_XUInstr)(UInstr* u)
 {
    non_fund_panic("SK_(pp_XUInstr)");
 }
 
+__attribute__ ((weak))
 Int SK_(get_Xreg_usage)(UInstr* u, Tag tag, RegUse* arr)
 {
    non_fund_panic("SK_(get_Xreg_usage)");
@@ -201,11 +221,13 @@ Int SK_(get_Xreg_usage)(UInstr* u, Tag tag, RegUse* arr)
    Syscall wrapping
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 void* SK_(pre_syscall)(ThreadId tid, UInt syscallno, Bool is_blocking)
 {
    non_fund_panic("SK_(pre_syscall)");
 }
 
+__attribute__ ((weak))
 void  SK_(post_syscall)(ThreadId tid, UInt syscallno,
                          void* pre_result, Int res, Bool is_blocking)
 {
@@ -216,6 +238,7 @@ void  SK_(post_syscall)(ThreadId tid, UInt syscallno,
    Shadow chunks
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 void SK_(complete_shadow_chunk)( ShadowChunk* sc, ThreadState* tst )
 {
    non_fund_panic("SK_(complete_shadow_chunk)");
@@ -225,6 +248,7 @@ void SK_(complete_shadow_chunk)( ShadowChunk* sc, ThreadState* tst )
    Alternative free()
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 void SK_(alt_free) ( ShadowChunk* sc, ThreadState* tst )
 {
    non_fund_panic("SK_(alt_free)");
@@ -234,11 +258,13 @@ void SK_(alt_free) ( ShadowChunk* sc, ThreadState* tst )
    Sanity checks
    ------------------------------------------------------------------ */
 
+__attribute__ ((weak))
 Bool SK_(cheap_sanity_check)(void)
 {
    non_fund_panic("SK_(cheap_sanity_check)");
 }
 
+__attribute__ ((weak))
 Bool SK_(expensive_sanity_check)(void)
 {
    non_fund_panic("SK_(expensive_sanity_check)");

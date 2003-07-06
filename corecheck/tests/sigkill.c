c3eadece35211120e7c04d2d199fb61352e76ac5
Dirk Mueller <daywalker@users.sourceforge.net>
2003-07-06 17:01:43 +0000

    make check more portable, patch by Matthew Emmerton
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@1730

--- corecheck/tests/sigkill.c
+++ corecheck/tests/sigkill.c
@@ -17,18 +17,19 @@ main (void)
   struct sigaction  sa;
 
   int i;
+  int rc;
   for (i = 1; i <= 65; i++) {
      sa.sa_flags   = 0;
      sigemptyset( &sa.sa_mask );
      sa.sa_handler = abend;
-     errno = 0;
      fprintf(stderr,"setting signal %d: ", i);
-     sigaction (i /*SIGKILL*/, &sa, NULL);
-     perror ("");
-     errno = 0;
+     rc = sigaction (i /*SIGKILL*/, &sa, NULL);
+     if (rc) perror ("");
+     else fprintf(stderr,"Success\n");
      fprintf(stderr,"getting signal %d: ", i);
-     sigaction (i /*SIGKILL*/, NULL, &sa);
-     perror ("");
+     rc = sigaction (i /*SIGKILL*/, NULL, &sa);
+     if (rc) perror ("");
+     else fprintf(stderr,"Success\n");
      fprintf(stderr,"\n");
   }
   return 0;

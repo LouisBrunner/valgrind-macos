f08f0f0ca36e206e017f8c998300387aaf36daea
Julian Seward <jseward@acm.org>
2003-09-17 21:48:26 +0000

    Suppress a couple more messages when -q applies.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@1821

--- memcheck/mac_leakcheck.c
+++ memcheck/mac_leakcheck.c
@@ -399,8 +399,10 @@ void MAC_(do_detect_memory_leaks) (
 
    if (lc_n_shadows == 0) {
       sk_assert(lc_shadows == NULL);
-      VG_(message)(Vg_UserMsg, 
-                   "No malloc'd blocks -- no leaks are possible.");
+      if (VG_(clo_verbosity) >= 1) {
+         VG_(message)(Vg_UserMsg, 
+                      "No malloc'd blocks -- no leaks are possible.");
+      }
       return;
    }
 

c639f03452657b2c75a8b934ee16449bd9d7ef05
Nicholas Nethercote <n.nethercote@gmail.com>
2003-10-30 10:33:30 +0000

    Fix cachegrind to deal with VG_(open)() returning any negative number on error, not just -1.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@1968

--- cachegrind/cg_main.c
+++ cachegrind/cg_main.c
@@ -1619,7 +1619,7 @@ static void fprint_BBCC_table_and_calc_totals(void)
 
    fd = VG_(open)(cachegrind_out_file, VKI_O_CREAT|VKI_O_TRUNC|VKI_O_WRONLY,
                                        VKI_S_IRUSR|VKI_S_IWUSR);
-   if (-1 == fd) {
+   if (fd < 0) {
       /* If the file can't be opened for whatever reason (conflict
          between multiple cachegrinded processes?), give up now. */
       file_err(); 

f4cf196dad1f84af5b686512c129d1276744e985
Jeremy Fitzhardinge <jeremy@valgrind.org>
2004-01-07 08:47:03 +0000

    Make badrw.c conform to C89; split things onto separate lines so it's clear what the messages are talking about.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@2192

--- memcheck/tests/badrw.c
+++ memcheck/tests/badrw.c
@@ -4,14 +4,26 @@ int main(void)
 {
    void* x = malloc(10);
 
-   int*        x4 = x-4;
-   short int*  x2 = x-4;
-   char*       x1 = x-1;
+   int   *x4;
+   short *x2;
+   char  *x1;
+   int    y4;
+   short  y2;
+   char   y1;
+
+   x4 = x-4;
+   x2 = x-4;
+   x1 = x-1;
 
    // Invalid reads and writes of sizes 4, 2, 1
-   int       y4 = *x4;  *x4 = y4;
-   short int y2 = *x2;  *x2 = y2;
-   char      y1 = *x1;  *x1 = y1;
+   y4 = *x4;
+   *x4 = y4;
+
+   y2 = *x2;
+   *x2 = y2;
+
+   y1 = *x1;
+   *x1 = y1;
    
    return 0;
 }

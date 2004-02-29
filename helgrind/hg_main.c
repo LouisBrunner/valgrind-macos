ffa1ae6f30cab9bc35916568c7fdc99ef343279f
Nicholas Nethercote <n.nethercote@gmail.com>
2004-02-29 15:50:04 +0000

    Patch from Bartosz Taudul:  latest GCC requires regparm(n) attributes on both function declarations and definitions.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@2280

--- helgrind/hg_main.c
+++ helgrind/hg_main.c
@@ -3103,42 +3103,42 @@ static void eraser_mem_write(Addr a, UInt size, ThreadId tid)
 
 #undef DEBUG_STATE
 
-static void eraser_mem_help_read_1(Addr a)
+REGPARM(1) static void eraser_mem_help_read_1(Addr a)
 {
    eraser_mem_read(a, 1, VG_(get_current_tid)());
 }
 
-static void eraser_mem_help_read_2(Addr a)
+REGPARM(1) static void eraser_mem_help_read_2(Addr a)
 {
    eraser_mem_read(a, 2, VG_(get_current_tid)());
 }
 
-static void eraser_mem_help_read_4(Addr a)
+REGPARM(1) static void eraser_mem_help_read_4(Addr a)
 {
    eraser_mem_read(a, 4, VG_(get_current_tid)());
 }
 
-static void eraser_mem_help_read_N(Addr a, UInt size)
+REGPARM(2) static void eraser_mem_help_read_N(Addr a, UInt size)
 {
    eraser_mem_read(a, size, VG_(get_current_tid)());
 }
 
-static void eraser_mem_help_write_1(Addr a, UInt val)
+REGPARM(2) static void eraser_mem_help_write_1(Addr a, UInt val)
 {
    if (*(UChar *)a != val)
       eraser_mem_write(a, 1, VG_(get_current_tid)());
 }
-static void eraser_mem_help_write_2(Addr a, UInt val)
+REGPARM(2) static void eraser_mem_help_write_2(Addr a, UInt val)
 {
    if (*(UShort *)a != val)
       eraser_mem_write(a, 2, VG_(get_current_tid)());
 }
-static void eraser_mem_help_write_4(Addr a, UInt val)
+REGPARM(2) static void eraser_mem_help_write_4(Addr a, UInt val)
 {
    if (*(UInt *)a != val)
       eraser_mem_write(a, 4, VG_(get_current_tid)());
 }
-static void eraser_mem_help_write_N(Addr a, UInt size)
+REGPARM(2) static void eraser_mem_help_write_N(Addr a, UInt size)
 {
    eraser_mem_write(a, size, VG_(get_current_tid)());
 }

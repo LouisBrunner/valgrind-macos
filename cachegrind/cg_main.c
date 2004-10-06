880d78e9c3ca60d907ce3f663a8df95452d82c27
Tom Hughes <tom@compton.nu>
2004-10-06 13:50:12 +0000

    Put back missing space...
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@2731

--- cachegrind/cg_main.c
+++ cachegrind/cg_main.c
@@ -829,7 +829,7 @@ static void fprint_CC_table_and_calc_totals(void)
    VG_(strcpy)(buf, "cmd:");
    VG_(write)(fd, (void*)buf, VG_(strlen)(buf));
    for (i = 0; i < VG_(client_argc); i++) {
-       VG_(write)(fd, "", 1);
+       VG_(write)(fd, " ", 1);
        VG_(write)(fd, VG_(client_argv)[i], VG_(strlen)(VG_(client_argv)[i]));
    }
    // "events:" line

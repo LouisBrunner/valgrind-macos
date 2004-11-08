a2954cd68b2bb77fa8eb2b198edf7fca150e3cdc
Nicholas Nethercote <n.nethercote@gmail.com>
2004-11-08 19:30:08 +0000

    Improved memcheck/tests/scalar so that it shows an example whereby the syscall number itself is undefined.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@2950

--- memcheck/tests/scalar.c
+++ memcheck/tests/scalar.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/syscall.h>
 
 int main(void)
 {
@@ -9,9 +10,12 @@ int main(void)
    // uninitialised, but we know pc[0] points to 0x0
    char** pc  = malloc(sizeof(char*));
    
-   // Four errors:  each of the scalar args are undefined, plus the 2nd arg
-   // points to unaddressable memory.
-   write(pi[0], pc[0], pi[0]+1);
+   // Five errors:  
+   // - the syscall number itself is undefined (but we know it's
+   //   0 + __NR_write :)
+   // - each of the scalar args are undefined
+   // - the 2nd arg points to unaddressable memory.
+   syscall(pi[0]+__NR_write, pi[0], pc[0], pi[0]+1);
 
    return 0;
 }

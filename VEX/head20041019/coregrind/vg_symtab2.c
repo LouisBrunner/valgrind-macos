2b3bb7f8a509182fa7118fd5f2cd4da5915b0b7a
Julian Seward <jseward@acm.org>
2004-11-10 17:24:44 +0000

    Provide our own versions of strchrnul and rawmemchr so we don't have to deal with glibc's ultra-optimised versions.
    --> CVS repo ?
    
    
    
    git-svn-id: svn://svn.valgrind.org/vex/trunk@539

--- VEX/head20041019/coregrind/vg_symtab2.c
+++ VEX/head20041019/coregrind/vg_symtab2.c
@@ -2521,6 +2521,10 @@ void VG_(setup_code_redirect_table) ( void )
                     "*vgpreload_memcheck.so*", "stpcpy");
    add_redirect_sym("soname:ld-linux.so.2", "strchr",
                     "*vgpreload_memcheck.so*", "strchr");
+   add_redirect_sym("soname:libc.so.6", "strchrnul",
+                    "*vgpreload_memcheck.so*", "glibc232_strchrnul");
+   add_redirect_sym("soname:libc.so.6", "rawmemchr",
+                    "*vgpreload_memcheck.so*", "glibc232_rawmemchr");
 }
 
 /*------------------------------------------------------------*/

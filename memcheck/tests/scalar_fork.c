4a6c06260893aa7549bb5a753a63ba4f0065c0a4
Nicholas Nethercote <n.nethercote@gmail.com>
2004-11-12 11:25:39 +0000

    CVS fixups: cvs add files I forgot to before;  add .cvsignore entries.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@2965

---
+++ memcheck/tests/scalar_fork.c
@@ -0,0 +1,13 @@
+#include "scalar.h"
+
+int main(void)
+{
+   // All __NR_xxx numbers are taken from x86
+   
+   // __NR_fork 2 --> arch/sys_fork()
+   GO(__NR_fork, "0e");
+   SY(__NR_fork);
+
+   return(0);
+}
+

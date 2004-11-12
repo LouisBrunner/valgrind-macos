ecdbfec7f1111cf092f6cb54444e93bc2c2b77f2
Julian Seward <jseward@acm.org>
2004-11-12 17:40:23 +0000

    Add copyright notices.
    git-svn-id: svn://svn.valgrind.org/vex/trunk@548

--- VEX/priv/main/vex_globals.h
+++ VEX/priv/main/vex_globals.h
@@ -6,6 +6,33 @@
 /*---                                                         ---*/
 /*---------------------------------------------------------------*/
 
+/*
+   This file is part of LibVEX, a library for dynamic binary
+   instrumentation and translation.
+
+   Copyright (C) 2004 OpenWorks, LLP.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; Version 2 dated June 1991 of the
+   license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or liability
+   for damages.  See the GNU General Public License for more details.
+
+   Neither the names of the U.S. Department of Energy nor the
+   University of California nor the names of its contributors may be
+   used to endorse or promote products derived from this software
+   without prior written permission.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA.
+*/
+
 #ifndef __VEX_GLOBALS_H
 #define __VEX_GLOBALS_H
 

825ce5be82846ca167c12ad5930f8c1cda2475eb
Tom Hughes <tom@compton.nu>
2004-11-15 16:59:18 +0000

    Pass an argument to exit_group in the scalar_exit_group test.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3010

--- memcheck/tests/scalar_exit_group.c
+++ memcheck/tests/scalar_exit_group.c
@@ -2,11 +2,15 @@
 
 int main(void)
 {
+   // uninitialised, but we know px[0] is 0x0
+   long* px  = malloc(sizeof(long));
+   long  x0  = px[0];
+
    // All __NR_xxx numbers are taken from x86
    
    // __NR_exit_group 252
    GO(__NR_exit_group, "1s 0m");
-   SY(__NR_exit_group);
+   SY(__NR_exit_group, x0);
 
    return(0);
 }

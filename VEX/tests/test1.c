c4e28a39546b66bb2f1e6650b8e0e66c37247302
Julian Seward <jseward@acm.org>
2004-11-18 15:38:37 +0000

    Nuke this useless bunch of tests.
    git-svn-id: svn://svn.valgrind.org/vex/trunk@566

--- VEX/tests/test1.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-
-int main ( int argc, char** argv )
-{
-  printf("testing, testing\n");
-  return 0;
-}
--- VEX/tests/test2b.c
+++
@@ -1,7 +0,0 @@
-
-#include <stdio.h>
-
-void test_printer ( int x )
-{
-  printf("test_printer: %d\n", x );
-}

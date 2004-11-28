46c0de784fd075422bf596bfb5368de433016fba
Julian Seward <jseward@acm.org>
2004-11-28 16:56:51 +0000

    Increase stated average translation size, at least for now.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3133

--- memcheck/mc_main.c
+++ memcheck/mc_main.c
@@ -1810,7 +1810,7 @@ void TL_(pre_clo_init)(void)
    VG_(details_copyright_author)(
       "Copyright (C) 2002-2004, and GNU GPL'd, by Julian Seward et al.");
    VG_(details_bug_reports_to)  (VG_BUGS_TO);
-   VG_(details_avg_translation_sizeB) ( 228 );
+   VG_(details_avg_translation_sizeB) ( 370 );
 
    VG_(needs_core_errors)         ();
    VG_(needs_tool_errors)         ();

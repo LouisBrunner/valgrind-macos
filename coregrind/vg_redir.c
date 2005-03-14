674d4934e5e49e2530cc3c15d5e3d4a25aadf2e0
Julian Seward <jseward@acm.org>
2005-03-14 19:25:49 +0000

    A few more suppressions and redirections to keep memcheck happy.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3357

--- coregrind/vg_redir.c
+++ coregrind/vg_redir.c
@@ -440,6 +440,8 @@ void VG_(setup_code_redirect_table) ( void )
 
    add_redirect_sym("soname:ld-linux.so.2", "stpcpy",
                     "*vgpreload_memcheck.so*", "stpcpy");
+   add_redirect_sym("soname:libc.so.6", "stpcpy",
+                    "*vgpreload_memcheck.so*", "stpcpy");
 
    add_redirect_sym("soname:libc.so.6", "strchr",
                     "*vgpreload_memcheck.so*", "strchr");

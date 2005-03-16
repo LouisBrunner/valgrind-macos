05cbc6b2e1ae3eb9d625206d4ff3ae26924f3dfb
Nicholas Nethercote <njn@valgrind.org>
2005-03-16 03:07:33 +0000

    Tweak manuel2 so that the Memcheck-detected error is more reliable, and that we never accidentally also get an undefined-condition-error.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3371

--- memcheck/tests/manuel2.c
+++ memcheck/tests/manuel2.c
@@ -3,7 +3,9 @@
 
 int main ()
 {
-  int *x;
+  int y = 0;
+  int *m = malloc(sizeof(int));
+  int *x = m[0] + &y;      // we know m[0] will be zero
 
   printf ("x = %d\n", *x==0xDEADBEEF ? 99 : 88);
 

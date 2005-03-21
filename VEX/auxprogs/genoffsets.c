d45a4802e117311f49e418d64da1eb2ffde422a7
Julian Seward <jseward@acm.org>
2005-03-21 17:41:45 +0000

    Generate offsets needed for V on amd64.
    git-svn-id: svn://svn.valgrind.org/vex/trunk@1067

--- VEX/auxprogs/genoffsets.c
+++ VEX/auxprogs/genoffsets.c
@@ -5,6 +5,7 @@
 
 #include "../pub/libvex_basictypes.h"
 #include "../pub/libvex_guest_x86.h"
+#include "../pub/libvex_guest_amd64.h"
 
 Int main ( void )
 {
@@ -31,6 +32,29 @@ Int main ( void )
 
   printf("#define OFFSET_x86_ESP %d\n", 
          offsetof(VexGuestX86State,guest_ESP));
+
+
+
+  printf("#define OFFSET_amd64_RAX %d\n", 
+         offsetof(VexGuestAMD64State,guest_RAX));
+
+  printf("#define OFFSET_amd64_RDI %d\n", 
+         offsetof(VexGuestAMD64State,guest_RDI));
+
+  printf("#define OFFSET_amd64_RSI %d\n", 
+         offsetof(VexGuestAMD64State,guest_RSI));
+
+  printf("#define OFFSET_amd64_RDX %d\n", 
+         offsetof(VexGuestAMD64State,guest_RDX));
+
+  printf("#define OFFSET_amd64_R8 %d\n", 
+         offsetof(VexGuestAMD64State,guest_R8));
+
+  printf("#define OFFSET_amd64_R9 %d\n", 
+         offsetof(VexGuestAMD64State,guest_R9));
+
+  printf("#define OFFSET_amd64_R10 %d\n", 
+         offsetof(VexGuestAMD64State,guest_R10));
 
   return 0;
 }

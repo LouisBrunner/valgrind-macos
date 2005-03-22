cdb6df5b597b41b9fda84c5244f1228e1ce218e9
Julian Seward <jseward@acm.org>
2005-03-22 02:43:14 +0000

    Track recent jmp_with_stack hackery.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3405

--- memcheck/tests/vgtest_ume.c
+++ memcheck/tests/vgtest_ume.c
@@ -134,7 +134,7 @@ static void test__do_exec(void)
 
 //   fprintf(stderr, "ume_go: %p %p\n", (void*)info.init_eip, (void*)esp);
 
-   jmp_with_stack(info.init_eip, (Addr)esp);
+   jump_and_switch_stacks((Addr)esp, info.init_eip);
 
    assert(0);  // UNREACHABLE
 }

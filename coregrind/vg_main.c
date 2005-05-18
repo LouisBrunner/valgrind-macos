abf27fd3b3a7007615a4d846b7c81241094bd82e
Nicholas Nethercote <njn@valgrind.org>
2005-05-18 04:36:01 +0000

    Hmm, actually put some code into m_options.c.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3766

--- coregrind/vg_main.c
+++ coregrind/vg_main.c
@@ -1395,55 +1395,6 @@ static void as_closepadfile(int padfile)
    vg_assert(0 == res);
 }
 
-
-/*====================================================================*/
-/*=== Command-line: variables, processing, etc                     ===*/
-/*====================================================================*/
-
-// See pub_{core,tool}_options.h for explanations of all these.
-
-/* Define, and set defaults. */
-VexControl VG_(clo_vex_control);
-Bool   VG_(clo_error_limit)    = True;
-Bool   VG_(clo_db_attach)      = False;
-Char*  VG_(clo_db_command)     = CLO_DEFAULT_DBCOMMAND;
-Int    VG_(clo_gen_suppressions) = 0;
-Int    VG_(clo_sanity_level)   = 1;
-Int    VG_(clo_verbosity)      = 1;
-Bool   VG_(clo_demangle)       = True;
-Bool   VG_(clo_trace_children) = False;
-Int    VG_(clo_log_fd)         = 2;
-Char*  VG_(clo_log_name)       = NULL;
-Bool   VG_(clo_time_stamp)     = False;
-Int    VG_(clo_input_fd)       = 0; /* stdin */
-Int    VG_(clo_n_suppressions) = 0;
-Char*  VG_(clo_suppressions)[VG_CLO_MAX_SFILES];
-Bool   VG_(clo_profile)        = False;
-UChar  VG_(clo_trace_flags)    = 0; // 00000000b
-UChar  VG_(clo_profile_flags)  = 0; // 00000000b
-Int    VG_(clo_trace_notbelow) = 0;
-Bool   VG_(clo_trace_syscalls) = False;
-Bool   VG_(clo_trace_signals)  = False;
-Bool   VG_(clo_trace_symtab)   = False;
-Bool   VG_(clo_trace_cfi)      = False;
-Bool   VG_(clo_trace_redir)    = False;
-Bool   VG_(clo_trace_sched)    = False;
-Bool   VG_(clo_trace_pthreads) = False;
-Int    VG_(clo_dump_error)     = 0;
-Int    VG_(clo_backtrace_size) = 12;
-Char*  VG_(clo_weird_hacks)    = NULL;
-Bool   VG_(clo_run_libc_freeres) = True;
-Bool   VG_(clo_track_fds)      = False;
-Bool   VG_(clo_show_below_main)= False;
-Bool   VG_(clo_pointercheck)   = True;
-Bool   VG_(clo_support_elan3)  = False;
-Bool   VG_(clo_branchpred)     = False;
-Bool   VG_(clo_model_pthreads) = False;
-Bool   VG_(clo_show_emwarns)   = False;
-Int    VG_(clo_max_stackframe) = 2000000;
-Bool   VG_(clo_wait_for_gdb)   = False;
-
-
 static void usage ( Bool debug_help )
 {
    Char* usage1 = 

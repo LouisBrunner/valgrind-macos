c2d4c4951a28341dc6c71634dee4cc3482367f71
Tom Hughes <tom@compton.nu>
2005-08-06 18:04:14 +0000

    Resync system call lists with the 2.6.13-rc5 kernel source.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@4335

--- coregrind/vki_unistd-ppc32-linux.h
+++ coregrind/vki_unistd-ppc32-linux.h
@@ -304,5 +304,8 @@
 //#define __NR_waitid		272
 //#define __NR_ioprio_set		273
 //#define __NR_ioprio_get		274
+//#define __NR_inotify_init	275
+//#define __NR_inotify_add_watch	276
+//#define __NR_inotify_rm_watch	277
 
 #endif /* __VKI_UNISTD_PPC32_LINUX_H */

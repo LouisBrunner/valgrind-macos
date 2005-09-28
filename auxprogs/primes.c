c35f8e62131b475ee0eaea467f1963489decf19d
Nicholas Nethercote <njn@valgrind.org>
2005-09-28 14:49:40 +0000

    Make primes.c a bazillion times faster.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@4810

--- auxprogs/primes.c
+++ auxprogs/primes.c
@@ -1,10 +1,12 @@
 
 #include <stdio.h>
+#include <math.h>
 
 int isprime ( int n )
 {
    int m;
-   for (m = 2; m < n; m++)
+   int sqrt_n = sqrt(n);
+   for (m = 2; m <= sqrt_n+1; m++)  // +1 in case of obscure rounding error
       if ((n % m) == 0) return 0;
    return 1;
 }

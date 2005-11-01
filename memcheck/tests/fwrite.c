030b1aac1009b9562d9e1e73809cc9d2db29cf64
Tom Hughes <tom@compton.nu>
2005-11-01 17:21:03 +0000

    Fixed test to not assume that malloc(10) will return 10 nul bytes!
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@4978

--- memcheck/tests/fwrite.c
+++ memcheck/tests/fwrite.c
@@ -1,9 +1,17 @@
-
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 int main ( void )
 {
    char* arr = malloc(10);
-   (void) write( 1 /* stdout */, arr, 10 );
+   int fd = open("/dev/null", O_WRONLY);
+   if (fd < 0) {
+      fprintf(stderr, "open failed\n");
+   } else {
+      (void)write(fd, arr, 10);
+      (void)close(fd);
+   }
+
    return 0;
 }

386b6b805a4786e0c40b52a8d575c85242782b56
Julian Seward <jseward@acm.org>
2005-11-05 14:08:35 +0000

    Read debug info from the V exe when it is installed as an in-place build.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@5020

--- coregrind/m_debuginfo/symtab.c
+++ coregrind/m_debuginfo/symtab.c
@@ -130,9 +130,12 @@ static void unload_symbols ( Addr start, SizeT length );
    that third segment, which is wrong and causes crashes.
 */
 
+/* Make a guess (doesn't have to be 100% correct) as to whether a path
+   is that of the valgrind exe we're using. */
 static Bool is_self ( HChar* filename )
 { 
-   return VG_(strstr)( filename, "/lib/valgrind/" ) != NULL;
+   return VG_(strstr)( filename, "/lib/valgrind/" ) != NULL
+          || VG_(strstr)( filename, ".in_place/" ) != NULL;
 }
 
 static void nuke_syms_in_range ( Addr start, SizeT length )

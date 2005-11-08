60df0c1a81c01e0fc39563a1b182765d6ecc895d
Julian Seward <jseward@acm.org>
2005-11-08 13:20:24 +0000

    Add tests for --partial-loads-ok= flag.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@5038

---
+++ memcheck/tests/partial_load.c
@@ -0,0 +1,45 @@
+
+#include <stdlib.h>
+#include <assert.h>
+
+int main ( void )
+{
+  long  w;
+  int   i;
+  char* p;
+
+  assert(sizeof(long) == sizeof(void*));
+
+  /* partial load, which --partial-loads-ok=yes should suppress */
+  p = calloc( sizeof(long)-1, 1 );
+  assert(p);
+  w = *(long*)p;
+  free(p);
+
+  /* partial but misaligned, cannot be suppressed */
+  p = calloc( sizeof(long), 1 );
+  assert(p);
+  p++;
+  w += *(long*)p;
+  p--;
+  free(p);
+
+  /* partial but not word sized, cannot be suppressed */
+  p = calloc( sizeof(short)-1, 1 );
+  assert(p);
+  w += (long)( *(short*)p );
+  free(p);
+
+  /* aligned, word sized, but completely invalid - cannot be suppressed */
+  p = calloc( sizeof(long), 1 );
+  assert(p);
+  free(p);
+  w += *(long*)p;
+
+  /* dump result in a way gcc can't understand */
+  for (i = 0; i < 64; i++)
+     w <<= 1;
+
+  return (int)w;
+}
+

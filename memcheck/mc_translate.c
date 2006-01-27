7ade90ed8ddda9b827c6310568a0d70c505a674f
Julian Seward <jseward@acm.org>
2006-01-27 15:11:31 +0000

    Track vex r1553 (addition of Iop_Est8FRecip, case Iop_Est5FRSqrt)
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@5596

--- memcheck/mc_translate.c
+++ memcheck/mc_translate.c
@@ -2185,6 +2185,8 @@ IRExpr* expr2vbits_Unop ( MCEnv* mce, IROp op, IRAtom* atom )
       case Iop_SqrtF64:
       case Iop_AbsF64:
       case Iop_2xm1F64:
+      case Iop_Est8FRecip:
+      case Iop_Est5FRSqrt:
       case Iop_Clz64:
       case Iop_Ctz64:
          return mkPCastTo(mce, Ity_I64, vatom);

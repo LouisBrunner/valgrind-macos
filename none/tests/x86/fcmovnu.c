fecb77b6e62c0169236d4d9347440bcc602758d6
Julian Seward <jseward@acm.org>
2006-03-06 19:50:06 +0000

    A test for fcmovnu.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@5715

---
+++ none/tests/x86/fcmovnu.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+double zzz;
+
+int main ( void )
+{
+  zzz = 1.234;
+  printf("zzz = %f\n", zzz);
+  __asm__ __volatile__(
+    "finit\n\t"
+    "fldpi\n\t"
+    "fldl2e\n\t"
+    "pushl %esi\n\t"
+    "movl $0,%esi\n\t"
+    "add %esi,%esi\n\t"
+    "fcmovnu %st(1), %st(0)\n\t"
+    "fstl zzz\n\t"
+    "finit\n\t"
+    "popl %esi\n\t"
+    );
+  printf("zzz = %f\n", zzz);
+  return 0;
+}

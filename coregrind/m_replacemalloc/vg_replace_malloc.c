3c371e5e7bdf9f07c82135dac47cf68edf05c517
Julian Seward <jseward@acm.org>
2006-05-21 01:52:09 +0000

    Back out r5595: the redirections deleted there are in fact used by gcc 2.96 on R H 7.3.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@5908

--- coregrind/m_replacemalloc/vg_replace_malloc.c
+++ coregrind/m_replacemalloc/vg_replace_malloc.c
@@ -150,6 +150,13 @@ ALLOC_or_NULL(m_libc_dot_so_star,      malloc,      malloc);
 //ALLOC_or_NULL(m_libpgc_dot_so,         malloc,      malloc);
 
 
+// operator new(unsigned int), not mangled (for gcc 2.96)
+ALLOC_or_BOMB(m_libstc_plus_plus_star, builtin_new,    __builtin_new);
+ALLOC_or_BOMB(m_libc_dot_so_star,      builtin_new,    __builtin_new);
+
+ALLOC_or_BOMB(m_libstc_plus_plus_star, __builtin_new,  __builtin_new);
+ALLOC_or_BOMB(m_libc_dot_so_star,      __builtin_new,  __builtin_new);
+
 // operator new(unsigned int), GNU mangling, 32-bit platforms
 // operator new(unsigned long), GNU mangling, 64-bit platforms
 #if VG_WORDSIZE == 4
@@ -177,6 +184,10 @@ ALLOC_or_NULL(m_libc_dot_so_star,      malloc,      malloc);
  ALLOC_or_NULL(m_libc_dot_so_star,      _ZnwmRKSt9nothrow_t,  __builtin_new);
 #endif
 
+
+// operator new[](unsigned int), not mangled (for gcc 2.96)
+ALLOC_or_BOMB(m_libstc_plus_plus_star, __builtin_vec_new, __builtin_vec_new );
+ALLOC_or_BOMB(m_libc_dot_so_star,      __builtin_vec_new, __builtin_vec_new );
 
 // operator new[](unsigned int), GNU mangling, 32-bit platforms
 // operator new[](unsigned long), GNU mangling, 64-bit platforms
@@ -225,6 +236,10 @@ FREE(m_libc_dot_so_star,       free,                 free );
 FREE(m_libstc_plus_plus_star,  cfree,                free );
 FREE(m_libc_dot_so_star,       cfree,                free );
 
+// operator delete(void*), not mangled (for gcc 2.96)
+FREE(m_libstc_plus_plus_star,  __builtin_delete,     __builtin_delete );
+FREE(m_libc_dot_so_star,       __builtin_delete,     __builtin_delete );
+
 // operator delete(void*), GNU mangling
 FREE(m_libstc_plus_plus_star,  _ZdlPv,               __builtin_delete );
 FREE(m_libc_dot_so_star,       _ZdlPv,               __builtin_delete );
@@ -232,6 +247,10 @@ FREE(m_libc_dot_so_star,       _ZdlPv,               __builtin_delete );
 // operator delete(void*, std::nothrow_t const&), GNU mangling
 FREE(m_libstc_plus_plus_star, _ZdlPvRKSt9nothrow_t,  __builtin_delete );
 FREE(m_libc_dot_so_star,      _ZdlPvRKSt9nothrow_t,  __builtin_delete );
+
+// operator delete[](void*), not mangled (for gcc 2.96)
+FREE(m_libstc_plus_plus_star,  __builtin_vec_delete, __builtin_vec_delete );
+FREE(m_libc_dot_so_star,       __builtin_vec_delete, __builtin_vec_delete );
 
 // operator delete[](void*), GNU mangling
 FREE(m_libstc_plus_plus_star,  _ZdaPv,               __builtin_vec_delete );

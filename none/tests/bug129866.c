e46821045c77a54f4cbca8e2de867c3c6eebbe98
Julian Seward <jseward@acm.org>
2006-08-16 15:07:48 +0000

    Regression test for fix for #129866.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@6005

---
+++ none/tests/bug129866.c
@@ -0,0 +1,34 @@
+
+#include <stdlib.h>
+char * touch_malloc (int size) 
+{
+  char * result;
+  int i;
+  result = malloc (size);
+  for (i = 0; i < size; i++)
+    *(result + i) = 'a';
+
+  return result;
+}
+char * touch_realloc (char * ptr, int size) 
+{
+  char * result;
+  int i;
+  result = realloc (ptr, size);
+  for (i = 0; i < size; i++)
+    *(result + i) = 'a';
+
+  return result;
+}
+
+int main ( void )
+{
+  char *a1, *b1, *a2, *b2;
+  printf("started\n");
+  a1 = touch_malloc(1600000) ;
+  b1 = touch_malloc(200000) ;
+  a2 = touch_realloc(a1, 1601600) ;
+  b2 = touch_realloc(b1, 200000) ;
+  printf("success\n");
+  return 0;
+};

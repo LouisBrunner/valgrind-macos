b155ab994e3524a4371ed843c2d136b8bdbc212e
Julian Seward <jseward@acm.org>
2006-08-28 13:24:08 +0000

    Merge r1637 (amd64 insn printing fix)
    git-svn-id: svn://svn.valgrind.org/vex/branches/VEX_3_2_BRANCH@1647

--- VEX/priv/host-amd64/isel.c
+++ VEX/priv/host-amd64/isel.c
@@ -172,7 +172,7 @@ static void addInstr ( ISelEnv* env, AMD64Instr* instr )
 {
    addHInstr(env->code, instr);
    if (vex_traceflags & VEX_TRACE_VCODE) {
-      ppAMD64Instr(instr, False);
+      ppAMD64Instr(instr, True);
       vex_printf("\n");
    }
 }

5c4ca351853aaae2f7f6c77330126f7d711ebd38
Julian Seward <jseward@acm.org>
2006-09-11 20:14:21 +0000

    Add a regtest for #133694 (munmapping of segments below aspacem_minAddr).  (Tom Hughes)
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@6050

---
+++ memcheck/tests/x86/bug133694.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+int main(int argc, char **argv)
+{
+   void *a1;
+   
+   if ((a1 = mmap((void *)0x200000, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS, -1, 0)) == MAP_FAILED)
+   {
+      perror("mmap1");
+      exit(1);
+   }
+
+   if (munmap(a1, 4096) < 0)
+   {
+      perror("munmap1");
+      exit(1);
+   }
+    
+   if (mmap((void *)0x100000, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS, -1, 0) == MAP_FAILED)
+   {
+      perror("mmap2");
+      exit(1);
+   }
+     
+   if (mmap((void *)0x100000, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) == MAP_FAILED)
+   {
+      perror("mmap2");
+      exit(1);
+   }
+
+   printf("success\n");
+   exit(0);
+}

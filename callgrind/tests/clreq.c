9418c0a710677e504b5b4c1ad0c8bccc6c0723cb
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2006-09-12 21:24:24 +0000

    Callgrind: Extend test for client requests
    This checks all of callgrinds client requests,
    and accordings actions (zero/dump) in different states
    (collection on/off, instrumentation on/off).
    
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@6058

--- callgrind/tests/clreq.c
+++ callgrind/tests/clreq.c
@@ -4,9 +4,63 @@
 
 #include "../callgrind.h"
 
+int some_work(int sum)
+{
+   int i;
+
+   for(i=0;i<10;i++) sum += i; /* some dummy work */
+
+   return sum;
+}
+
+int some_callgrind_clreqs()
+{
+   int sum = some_work(0);
+
+   CALLGRIND_ZERO_STATS;
+
+   sum += some_work(sum);
+
+   CALLGRIND_DUMP_STATS;
+
+   sum += some_work(sum);
+
+   CALLGRIND_DUMP_STATS_AT("Please dump here");
+
+   return some_work(sum);
+}
+
+int more_callgrind_clreqs()
+{
+   int sum = some_callgrind_clreqs();
+
+   CALLGRIND_TOGGLE_COLLECT;
+
+   sum += some_callgrind_clreqs();
+
+   CALLGRIND_TOGGLE_COLLECT;
+
+   return sum;
+}
+
+   
 int main(void)
 {
-   CALLGRIND_ZERO_STATS;
+   more_callgrind_clreqs();
+
+   CALLGRIND_STOP_INSTRUMENTATION;
+
+   more_callgrind_clreqs();
+
+   CALLGRIND_START_INSTRUMENTATION;
+
+   more_callgrind_clreqs();
+
+   CALLGRIND_STOP_INSTRUMENTATION;
+
+   more_callgrind_clreqs();
+
+   CALLGRIND_START_INSTRUMENTATION;
 
    return RUNNING_ON_VALGRIND;
 }

09c2f81371dcefc08a5d9243c67c8faa579ee4da
Julian Seward <jseward@acm.org>
2007-04-07 12:15:44 +0000

    Test sahf/lahf on amd64.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@6695

---
+++ none/tests/amd64/slahf-amd64.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+
+typedef unsigned long long int ULong;
+
+extern ULong lahf_1 ( void );
+asm("\n"
+"lahf_1:\n"
+"\tpushq $0\n"
+"\tpopfq\n"
+"\tmovabsq $0xFFFFFFFFFFFFFFFF, %rax\n"
+"\tmovabsq $0x8765432112345678, %rdx\n"
+"\tsubq %rax, %rdx\n"
+"\tlahf\n"
+"\tret\n"
+);
+
+extern ULong lahf_0 ( void );
+asm("\n"
+"lahf_0:\n"
+"\tpushq $0\n"
+"\tpopfq\n"
+"\tmovabsq $0x0, %rax\n"
+"\tmovabsq $0x8765432112345678, %rdx\n"
+"\tsubq %rax, %rdx\n"
+"\tlahf\n"
+"\tret\n"
+);
+
+extern ULong sahf_then_lahf ( ULong );
+asm("\n"
+"sahf_then_lahf:\n"
+"\tmovq %rdi, %rax\n"
+"\tsahf\n"
+"\tmovabsq $0, %rax\n"
+"\tlahf\n"
+"\tret\n"
+);
+
+int main ( void )
+{
+   ULong i;
+   printf("0x%llx\n", lahf_0());
+   printf("0x%llx\n", lahf_1());
+   for (i = 0; i < 255; i++) {
+      ULong x = sahf_then_lahf(i << 8);
+      printf("%llx -> %llx\n", i, x);
+   }
+   return 0;
+}

4478efba982c49a3f46566f434c5f336c9ed08be
Julian Seward <jseward@acm.org>
2007-11-09 23:16:11 +0000

    Oops, this was missed out of r7118 (Merge (from branches/THRCHECK) the following amd64-linux stack unwind kludges)
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@7122

--- include/pub_tool_stacktrace.h
+++ include/pub_tool_stacktrace.h
@@ -37,8 +37,11 @@ typedef Addr* StackTrace;
 // Walks the stack to get instruction pointers from the top stack frames for
 // thread 'tid'.  Maximum of 'n_ips' addresses put into 'ips';  0 is the top
 // of the stack, 1 is its caller, etc.  Everything from ips[n_ips] onwards
-// is undefined and should not be read.
-extern UInt VG_(get_StackTrace) ( ThreadId tid, StackTrace ips, UInt n_ips );
+// is undefined and should not be read.  The initial IP value to 
+// use is adjusted by first_ip_delta before the stack is unwound.
+// A safe value to pass is zero.
+extern UInt VG_(get_StackTrace) ( ThreadId tid, StackTrace ips, UInt n_ips,
+                                  Word first_ip_delta );
 
 // Apply a function to every element in the StackTrace.  The parameter 'n'
 // gives the index of the passed ip.  Doesn't go below main() unless

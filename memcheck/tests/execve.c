eab41712851dee104746373d574d74691645c44d
Tom Hughes <tom@compton.nu>
2008-01-05 00:01:20 +0000

    Fix compiler warnings by using a variable to hide the null pointer from the compiler.
    Patch from Bart Van Assche <bart.vanassche@gmail.com>.
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@7317

--- memcheck/tests/execve.c
+++ memcheck/tests/execve.c
@@ -2,10 +2,11 @@
 
 int main(void)
 {
+   char* null_filename = NULL;
    char* bad[2]  = { (char*)1, NULL };
    char* good[1] = {           NULL };
 
-   execve(NULL, bad, bad);
+   execve(null_filename, bad, bad);
    execve("/bin/true", good, good);
 
    return 0;
--- memcheck/tests/execve2.c
+++ memcheck/tests/execve2.c
@@ -3,7 +3,9 @@
 
 int main ( int argc, char** argv, char** envp )
 {
-   execve(NULL,        NULL, NULL);
+   char* null_filename = NULL;
+
+   execve(null_filename,      NULL, NULL);
    execve("../../tests/true", NULL, envp);
    assert(0);  // shouldn't get here
 }

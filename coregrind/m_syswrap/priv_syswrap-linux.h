cbb3cb05a567bc8ba7b9736b0d1ddabb7a952a40
Tom Hughes <tom@compton.nu>
2008-01-08 16:31:25 +0000

    Add support for the sync_file_range system call.
    Based on patch from Russell Sears <sears@cs.berkeley.edu>.
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@7324

--- coregrind/m_syswrap/priv_syswrap-linux.h
+++ coregrind/m_syswrap/priv_syswrap-linux.h
@@ -220,6 +220,9 @@ DECL_TEMPLATE(linux, sys_rt_sigtimedwait);
 DECL_TEMPLATE(linux, sys_rt_sigqueueinfo);
 DECL_TEMPLATE(linux, sys_rt_sigsuspend);
 
+// Linux-specific?
+DECL_TEMPLATE(linux, sys_sync_file_range);
+
 /* ---------------------------------------------------------------------
    Wrappers for sockets and ipc-ery.  These are split into standalone
    procedures because x86-linux hides them inside multiplexors

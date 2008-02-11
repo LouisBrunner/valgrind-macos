2a9e795be77d8ddc557b6072c966dd8b31606f43
Julian Seward <jseward@acm.org>
2008-02-11 11:35:40 +0000

    Update copyright dates ("200X-2007" --> "200X-2008").
    git-svn-id: svn://svn.valgrind.org/vex/trunk@1811

--- VEX/priv/main/vex_util.h
+++ VEX/priv/main/vex_util.h
@@ -10,7 +10,7 @@
    This file is part of LibVEX, a library for dynamic binary
    instrumentation and translation.
 
-   Copyright (C) 2004-2007 OpenWorks LLP.  All rights reserved.
+   Copyright (C) 2004-2008 OpenWorks LLP.  All rights reserved.
 
    This library is made available under a dual licensing scheme.
 

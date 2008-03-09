8033600db46edf917c79561b88a180bd7430b570
Bart Van Assche <bvanassche@acm.org>
2008-03-09 16:16:06 +0000

    Added test for LinuxThreads detection.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@7616

---
+++ exp-drd/tests/linuxthreads_det.c
@@ -0,0 +1,41 @@
+/** Test whether DRD recognizes LinuxThreads as LinuxThreads and NPTL as
+ *  NPTL.
+ */
+
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+static sem_t s_sem;
+static pid_t s_main_thread_pid;
+
+
+void* thread_func(void* arg)
+{
+  if (s_main_thread_pid == getpid())
+  {
+    printf("NPTL or non-Linux POSIX threads implemenentation detected.\n");
+  }
+  else
+  {
+    printf("Detected LinuxThreads as POSIX threads implemenentation.\n");
+  }
+  sem_post(&s_sem);
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+  pthread_t threadid;
+
+  s_main_thread_pid = getpid();
+  sem_init(&s_sem, 0, 0);
+  pthread_create(&threadid, 0, thread_func, 0);
+  sem_wait(&s_sem);
+  pthread_join(threadid, 0);
+  sem_destroy(&s_sem);
+  return 0;
+}

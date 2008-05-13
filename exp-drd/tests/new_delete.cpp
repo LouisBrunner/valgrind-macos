1d7535150596faf850ff0b42cb81526fa8021cac
Bart Van Assche <bvanassche@acm.org>
2008-05-13 16:16:14 +0000

    Added allocation and deallocation of zero-sized array.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@8060

--- exp-drd/tests/new_delete.cpp
+++ exp-drd/tests/new_delete.cpp
@@ -4,10 +4,14 @@
 
 int main(int argc, char** argv)
 {
+  int zero = 0;
   int* p = new int;
   int* q = new int[733];
   delete[] q;
   delete p;
+
+  q = new int[zero];
+  delete q;
 
   fprintf(stderr, "Success.\n");
 

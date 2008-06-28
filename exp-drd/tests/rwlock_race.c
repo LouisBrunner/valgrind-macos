58985b01c327083e6870898ef07f018615d84ddc
Bart Van Assche <bvanassche@acm.org>
2008-06-28 11:53:01 +0000

    Delayed thread termination such that the "other segment" information is included in the data race reports.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@8294

--- exp-drd/tests/rwlock_race.c
+++ exp-drd/tests/rwlock_race.c
@@ -8,19 +8,27 @@
 
 #define _GNU_SOURCE 1
 
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "../drd_clientreq.h"
 
 
 static pthread_rwlock_t s_rwlock;
 static int s_racy;
 
-static void* thread(void* arg)
+static void sleep_ms(const int ms)
+{
+  struct timespec delay = { ms / 1000, (ms % 1000) * 1000 * 1000 };
+  nanosleep(&delay, 0);
+}
+
+static void* thread_func(void* arg)
 {
   pthread_rwlock_rdlock(&s_rwlock);
   s_racy++;
   pthread_rwlock_unlock(&s_rwlock);
+  sleep_ms(100);
   return 0;
 }
 
@@ -36,8 +44,8 @@ int main(int argc, char** argv)
 #endif
 
   pthread_rwlock_init(&s_rwlock, 0);
-  pthread_create(&thread1, 0, thread, 0);
-  pthread_create(&thread2, 0, thread, 0);
+  pthread_create(&thread1, 0, thread_func, 0);
+  pthread_create(&thread2, 0, thread_func, 0);
   pthread_join(thread1, 0);
   pthread_join(thread2, 0);
   pthread_rwlock_destroy(&s_rwlock);

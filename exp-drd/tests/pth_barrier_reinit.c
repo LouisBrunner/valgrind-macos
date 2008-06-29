77782ec3346a2b7c9624b3f9bed222c08b312bf4
Bart Van Assche <bvanassche@acm.org>
2008-06-29 07:20:00 +0000

    Added #define _GNU_SOURCE to make sure that this test compiles with older POSIX threads implementations.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@8305

--- exp-drd/tests/pth_barrier_reinit.c
+++ exp-drd/tests/pth_barrier_reinit.c
@@ -1,4 +1,12 @@
+/** Trigger barrier reinitialization, which is not allowed by the POSIX
+ *  threads standard. See also http://www.opengroup.org/onlinepubs/000095399/functions/pthread_barrier_init.html.
+ */
+
+
+#define _GNU_SOURCE
+
 #include <pthread.h>
+
 
 int main(int argc, char** argv)
 {

a4ddba81ca7b528524894cdfd796f9968cf3169e
Bart Van Assche <bvanassche@acm.org>
2008-11-10 18:54:46 +0000

    Added regression test for Boost.Threads.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@8763

---
+++ drd/tests/boost_threads.cpp
@@ -0,0 +1,30 @@
+#include <boost/thread/condition.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+#include <iostream>
+
+
+static boost::condition s_cva;
+static boost::mutex s_m;
+
+
+static void thread_func(void)
+{
+  std::cerr << "Thread 2.\n";
+  boost::mutex::scoped_lock sl(s_m);
+  s_cva.notify_all();
+  s_cva.wait(sl);
+}
+
+int main(int argc, char** argv)
+{
+  std::cerr << "Thread 1.\n";
+  boost::mutex::scoped_lock sl(s_m);
+  boost::thread t(thread_func);
+  s_cva.wait(sl);
+  s_cva.notify_all();
+  sl.unlock();
+  t.join();
+  std::cerr << "Finished.\n";
+  return 0;
+}

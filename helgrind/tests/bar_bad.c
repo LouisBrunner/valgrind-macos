2ce349dd7bb4d493be9fc5e1959271ae8400e070
Julian Seward <jseward@acm.org>
2008-11-14 19:43:44 +0000

    Add some regression tests for pthread_barrier handling.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@8768

---
+++ helgrind/tests/bar_bad.c
@@ -0,0 +1,89 @@
+
+/* This program checks that Helgrind reports the five degenerate
+   uses of the barrier functions shown. */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+void* child1 ( void* arg )
+{
+   pthread_barrier_wait( (pthread_barrier_t*)arg );
+   return NULL;
+}
+
+int main ( void )
+{
+  pthread_barrier_t *bar1, *bar2, *bar3, *bar4, *bar5;
+  pthread_t thr1, thr2;
+  int r;
+
+  /* possibly set up a watchdog timer thread here. */
+
+
+
+
+
+
+
+
+
+
+
+  /* initialise a barrier with a zero count */
+  fprintf(stderr, "\ninitialise a barrier with zero count\n");
+  bar1 = malloc(sizeof(pthread_barrier_t));
+  pthread_barrier_init(bar1, NULL, 0);
+
+  /* initialise a barrier twice */
+  fprintf(stderr, "\ninitialise a barrier twice\n");
+  bar2 = malloc(sizeof(pthread_barrier_t));
+  pthread_barrier_init(bar2, NULL, 1);
+  pthread_barrier_init(bar2, NULL, 1);
+
+  /* initialise a barrier which has threads waiting on it.
+     This isn't too simple. */
+  fprintf(stderr, "\ninitialise a barrier which has threads waiting on it\n");
+  bar3 = malloc(sizeof(pthread_barrier_t));
+  pthread_barrier_init(bar3, NULL, 2);
+  /* create a thread, whose only purpose is to block on the barrier */
+  pthread_create(&thr1, NULL, child1, (void*)bar3);
+  /* guarantee that it gets there first */
+  sleep(1);
+  /* and now reinitialise */
+  pthread_barrier_init(bar3, NULL, 3);
+
+  /* destroy a barrier that has threads waiting at it */
+  fprintf(stderr, "\ndestroy a barrier that has waiting threads\n");
+  /* once again, create a thread, whose only purpose is to block. */
+  bar4 = malloc(sizeof(pthread_barrier_t));
+  pthread_barrier_init(bar4, NULL, 2);
+  /* create a thread, whose only purpose is to block on the barrier */
+  pthread_create(&thr2, NULL, child1, (void*)bar4);
+  /* guarantee that it gets there first */
+  sleep(1);
+  /* and now destroy */
+  pthread_barrier_destroy(bar4);
+
+  /* destroy a barrier that was never initialised.  This is a bit
+     tricky, in that we have to fill the barrier with bytes which
+     ensure that the pthread_barrier_destroy call doesn't hang for
+     some reason.  Zero-fill seems to work ok on amd64-linux (glibc
+     2.8). */
+  fprintf(stderr, "\ndestroy a barrier that was never initialised\n");
+  bar5 = malloc(sizeof(pthread_barrier_t));
+  assert(bar5);
+  memset(bar5, 0, sizeof(*bar5));
+  pthread_barrier_destroy(bar5);
+
+  /* now we need to clean up the mess .. */
+  r= pthread_cancel(thr1); assert(!r);
+  r= pthread_cancel(thr2); assert(!r);
+
+  free(bar1); free(bar2); free(bar3); free(bar4); free(bar5);
+
+  return 0;
+}
---
+++ helgrind/tests/bar_trivial.c
@@ -0,0 +1,57 @@
+
+/* This is the most trivial test I could think of that involves
+   barriers.  If H fails to notice the pthread_barrier_wait call then
+   it will report a race.  Correct behaviour is not to report a race
+   (there isn't one.) */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
+int x = 0;
+
+pthread_barrier_t bar;
+
+void* child_fn ( void* arg )
+{
+   long r, n = (long)arg;
+
+   if (n == 1) x++;
+
+   r = pthread_barrier_wait(&bar); 
+   assert(r == 0 || r == PTHREAD_BARRIER_SERIAL_THREAD);
+
+   if (n == 0) x++;
+
+   sleep(1); /* ensure both threads get to this point before
+                either exits. */
+   return NULL;
+}
+
+#define NTHR 2
+
+int main ( void )
+{
+   long i, r;
+   pthread_t thr[NTHR];
+
+   r = pthread_barrier_init(&bar, NULL, NTHR);
+   assert(!r);
+
+   for (i = 0; i < NTHR; i++) {
+      r = pthread_create(&thr[i], NULL, child_fn, (void*)i);
+      assert(!r);
+   }
+
+   for (i = 0; i < NTHR; i++) {
+      r = pthread_join(thr[i], NULL);
+      assert(!r);
+   }
+
+   r = pthread_barrier_destroy(&bar); assert(!r);
+
+   printf("x = %d\n", x);
+   return 0;
+}

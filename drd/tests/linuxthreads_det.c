092f96902c8a3780321c1a002e41d6158d5debb7
Bart Van Assche <bvanassche@acm.org>
2008-11-27 17:48:36 +0000

    Simplified test program.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@8802

--- drd/tests/linuxthreads_det.c
+++ drd/tests/linuxthreads_det.c
@@ -8,7 +8,6 @@
 #include <unistd.h>
 
 
-static sem_t s_sem;
 static pid_t s_main_thread_pid;
 
 
@@ -22,7 +21,6 @@ void* thread_func(void* arg)
   {
     write(STDOUT_FILENO, "Detected LinuxThreads as POSIX threads implementation.\n", 55);
   }
-  sem_post(&s_sem);
   return 0;
 }
 
@@ -31,10 +29,7 @@ int main(int argc, char** argv)
   pthread_t threadid;
 
   s_main_thread_pid = getpid();
-  sem_init(&s_sem, 0, 0);
   pthread_create(&threadid, 0, thread_func, 0);
-  sem_wait(&s_sem);
   pthread_join(threadid, 0);
-  sem_destroy(&s_sem);
   return 0;
 }

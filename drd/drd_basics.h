27f484bc70b379884f798c00877a3813d26be29c
Bart Van Assche <bvanassche@acm.org>
2009-02-14 10:14:09 +0000

    Added file to repository.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@9146

---
+++ drd/drd_basics.h
@@ -0,0 +1,38 @@
+/*
+  This file is part of DRD, a data race detector.
+
+  Copyright (C) 2009 Bart Van Assche <bart.vanassche@gmail.com>.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+  02111-1307, USA.
+
+  The GNU General Public License is contained in the file COPYING.
+*/
+
+
+/* Basic definitions needed by (almost) all DRD source files. */
+
+
+#ifndef __DRD_BASICS_H
+#define __DRD_BASICS_H
+
+
+#include "pub_tool_basics.h"      // Addr
+
+
+#define DRD_(str) VGAPPEND(vgDrd_, str)
+
+
+#endif /* __DRD_BASICS_H */

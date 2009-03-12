190ad4143eb79e5f28b037d23b8022f4a2e907b4
Nicholas Nethercote <njn@valgrind.org>
2009-03-12 03:00:05 +0000

    - Removed leakotron.  It was not a very useful test, and hard to make   portable. - Rewrote leak-tree to be deterministic and clearer.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@9372

--- memcheck/tests/leak-tree.c
+++ memcheck/tests/leak-tree.c
@@ -1,46 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "leak.h"
 #include "../memcheck.h"
 
-struct foo {
-	struct foo *l, *r;
-};
+/* We build this tree:
+  
+           A
+         /   \
+        B     C
+       / \   / \ 
+      D   E F   G
+  
+   Then we leak D and C-F-G.
+*/
 
-struct foo *mk(struct foo *l, struct foo *r)
+typedef
+   struct _Node {
+      struct _Node *l;
+      struct _Node *r;
+      // Padding ensures the structu is the same size on 32-bit and 64-bit
+      // machines.
+      char padding[16 - 2*sizeof(struct _Node*)];
+   } Node;
+
+Node* mk(void)
 {
-	struct foo *f = malloc(sizeof(*f));
-	f->l = l;
-	f->r = r;
-
-	return f;
+   Node *x = malloc(sizeof(Node));
+   x->l = NULL;
+   x->r = NULL;
+   return x;
 }
 
-int main()
+// This is a definite root.
+Node* t;
+
+void f(void)
 {
-	struct foo *volatile a, *volatile b, *volatile c;
+   // Building like this rather than "t = mk(mk(mk(NULL, NULL), ...)" seems to
+   // help avoid leaving pointers on the stack to supposedly-leaked blocks.
+   t       = mk();   // A
+   t->l    = mk();   // B
+   t->r    = mk();   // C  (48(16d,32i)/1 definitely leaked from here)
+   t->l->l = mk();   // D  (16/1 definitely leaked from here)
+   t->l->r = mk();   // E
+   t->r->l = mk();   // F
+   t->r->r = mk();   // G
 
-	b = mk(mk(0, 0), 0);
-
-	/* Partially leaked tree structure */
-	a = mk(mk(mk(mk(mk(0, 0), 0), mk(mk(0, 0), b)), mk(0, 0)), mk(0,0));
-		   
-	a = NULL;
-
-	VALGRIND_DO_LEAK_CHECK;
-
-	b = NULL;		/* now leak old b */
-
-	/* Completely leaked, but part of the structure is shared */
-	b = mk(0,0);
-	b->l = b;
-	b->r = b;
-
-	a = mk(b,b);
-	c = mk(b,b);
-
-	a = b = c = 0;
-
-	VALGRIND_DO_LEAK_CHECK;
-
-	return 0;
+   // Sever B->D, leaking D
+   t->l->l = NULL;
+ 
+   // Sever A->C, leaking C-F-G
+   t->r = NULL;
 }
+
+int main(void)
+{
+   DECLARE_LEAK_COUNTERS;
+
+   GET_INITIAL_LEAK_COUNTS;
+
+   // See leak-cases.c for why we do the work in f().
+   f();
+
+   GET_FINAL_LEAK_COUNTS;
+
+   PRINT_LEAK_COUNTS(stderr);
+
+   return 0;
+}
+

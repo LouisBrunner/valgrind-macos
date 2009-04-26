ca8a77d31b1ec95f7172ed69ae1fd1149fd18b48
Bart Van Assche <bvanassche@acm.org>
2009-04-26 07:14:15 +0000

    Added regression test pth_create_glibc_2_0.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@9633

---
+++ drd/tests/pth_create_glibc_2_0.c
@@ -0,0 +1,34 @@
+/* Test program that invokes pthread_create@GLIBC_2.0(). */
+
+
+#include <pthread.h>
+#include <stdio.h>
+
+
+extern int pthread_create_glibc_2_0(pthread_t*, const pthread_attr_t*,
+                                    void *(*)(void*), void*);
+
+__asm__(".symver pthread_create_glibc_2_0, pthread_create@GLIBC_2.0");
+
+
+static void* thread_func(void *arg)
+{
+  fprintf(stderr, "The thread.\n");
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+  int result;
+  pthread_t thr;
+
+  result = (*pthread_create_glibc_2_0)(&thr, 0, thread_func, 0);
+  if (result != 0)
+  {
+    fprintf(stderr, "pthread_create() failed.\n");
+    return 1;
+  }
+  pthread_join(thr, 0);
+  fprintf(stderr, "Finished.\n");
+  return 0;
+}

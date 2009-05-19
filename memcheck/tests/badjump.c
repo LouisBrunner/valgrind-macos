9551ec33fab9f647c573f9283c75114285faf639
Nicholas Nethercote <njn@valgrind.org>
2009-05-19 00:54:19 +0000

    Minor trunk/DARWIN sync.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@9911

--- memcheck/tests/badjump.c
+++ memcheck/tests/badjump.c
@@ -5,8 +5,8 @@ int main ( void )
 #if defined(__powerpc64__) || defined(_AIX)
    /* on ppc64-linux, a function pointer points to a function
       descriptor, not to the function's entry point.  Hence to get
-      uniform behaviour on all supported targets - a jump to 0xE000000
-      - the following is needed. */
+      uniform behaviour on all supported targets - a jump to an
+      unmapped page - the following is needed. */
    unsigned long long int fake_fndescr[3];
    fake_fndescr[0] = (unsigned long long int)get_unmapped_page();
    fake_fndescr[1] = 0;

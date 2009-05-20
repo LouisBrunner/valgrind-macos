70f8bb547ebf4a77f5ef6575cb7a69e3b98d062b
Nicholas Nethercote <njn@valgrind.org>
2009-05-20 06:46:25 +0000

    DARWIN sync: remove unused VG_UCONTEXT_SYSCALL_NUM.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@10037

--- coregrind/m_signals.c
+++ coregrind/m_signals.c
@@ -267,7 +267,6 @@ typedef struct SigQueue {
 #  define VG_UCONTEXT_INSTR_PTR(uc)       ((uc)->uc_mcontext.eip)
 #  define VG_UCONTEXT_STACK_PTR(uc)       ((uc)->uc_mcontext.esp)
 #  define VG_UCONTEXT_FRAME_PTR(uc)       ((uc)->uc_mcontext.ebp)
-#  define VG_UCONTEXT_SYSCALL_NUM(uc)     ((uc)->uc_mcontext.eax)
 #  define VG_UCONTEXT_SYSCALL_SYSRES(uc)                        \
       /* Convert the value in uc_mcontext.eax into a SysRes. */ \
       VG_(mk_SysRes_x86_linux)( (uc)->uc_mcontext.eax )
@@ -277,7 +276,6 @@ typedef struct SigQueue {
 #  define VG_UCONTEXT_INSTR_PTR(uc)       ((uc)->uc_mcontext.rip)
 #  define VG_UCONTEXT_STACK_PTR(uc)       ((uc)->uc_mcontext.rsp)
 #  define VG_UCONTEXT_FRAME_PTR(uc)       ((uc)->uc_mcontext.rbp)
-#  define VG_UCONTEXT_SYSCALL_NUM(uc)     ((uc)->uc_mcontext.rax)
 #  define VG_UCONTEXT_SYSCALL_SYSRES(uc)                        \
       /* Convert the value in uc_mcontext.rax into a SysRes. */ \
       VG_(mk_SysRes_amd64_linux)( (uc)->uc_mcontext.rax )
@@ -331,7 +329,6 @@ typedef struct SigQueue {
 #  define VG_UCONTEXT_INSTR_PTR(uc)       ((uc)->uc_regs->mc_gregs[VKI_PT_NIP])
 #  define VG_UCONTEXT_STACK_PTR(uc)       ((uc)->uc_regs->mc_gregs[VKI_PT_R1])
 #  define VG_UCONTEXT_FRAME_PTR(uc)       ((uc)->uc_regs->mc_gregs[VKI_PT_R1])
-#  define VG_UCONTEXT_SYSCALL_NUM(uc)     ((uc)->uc_regs->mc_gregs[VKI_PT_R0])
 #  define VG_UCONTEXT_SYSCALL_SYSRES(uc)                            \
       /* Convert the values in uc_mcontext r3,cr into a SysRes. */  \
       VG_(mk_SysRes_ppc32_linux)(                                   \
@@ -344,7 +341,6 @@ typedef struct SigQueue {
 #  define VG_UCONTEXT_INSTR_PTR(uc)       ((uc)->uc_mcontext.gp_regs[VKI_PT_NIP])
 #  define VG_UCONTEXT_STACK_PTR(uc)       ((uc)->uc_mcontext.gp_regs[VKI_PT_R1])
 #  define VG_UCONTEXT_FRAME_PTR(uc)       ((uc)->uc_mcontext.gp_regs[VKI_PT_R1])
-#  define VG_UCONTEXT_SYSCALL_NUM(uc)     ((uc)->uc_mcontext.gp_regs[VKI_PT_R0])
    /* Dubious hack: if there is an error, only consider the lowest 8
       bits of r3.  memcheck/tests/post-syscall shows a case where an
       interrupted syscall should have produced a ucontext with 0x4
@@ -378,12 +374,6 @@ typedef struct SigQueue {
       struct mstsave* jc = &mc->jmp_context;
       return jc->gpr[1];
    }
-   static inline Addr VG_UCONTEXT_SYSCALL_NUM( void* ucV ) {
-      ucontext_t* uc = (ucontext_t*)ucV;
-      struct __jmpbuf* mc = &(uc->uc_mcontext);
-      struct mstsave* jc = &mc->jmp_context;
-      return jc->gpr[2];
-   }
    static inline SysRes VG_UCONTEXT_SYSCALL_SYSRES( void* ucV ) {
       ucontext_t* uc = (ucontext_t*)ucV;
       struct __jmpbuf* mc = &(uc->uc_mcontext);
@@ -416,12 +406,6 @@ typedef struct SigQueue {
       struct __jmpbuf* mc = &(uc->uc_mcontext);
       struct __context64* jc = &mc->jmp_context;
       return jc->gpr[1];
-   }
-   static inline Addr VG_UCONTEXT_SYSCALL_NUM( void* ucV ) {
-      ucontext_t* uc = (ucontext_t*)ucV;
-      struct __jmpbuf* mc = &(uc->uc_mcontext);
-      struct __context64* jc = &mc->jmp_context;
-      return jc->gpr[2];
    }
    static inline SysRes VG_UCONTEXT_SYSCALL_SYSRES( void* ucV ) {
       ucontext_t* uc = (ucontext_t*)ucV;

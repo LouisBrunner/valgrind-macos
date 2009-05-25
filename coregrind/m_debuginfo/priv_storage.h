f7b44686d5832b939b79a512fcdae4922558f31f
Nicholas Nethercote <njn@valgrind.org>
2009-05-25 01:48:32 +0000

    DARWIN sync: add a comment.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@10149

--- coregrind/m_debuginfo/priv_storage.h
+++ coregrind/m_debuginfo/priv_storage.h
@@ -51,6 +51,10 @@ typedef
       Addr  addr;   /* lowest address of entity */
       Addr  tocptr; /* ppc64-linux only: value that R2 should have */
       UChar *name;  /* name */
+      // XXX: this could be shrunk (on 32-bit platforms) by using 31 bits for
+      // the size and 1 bit for the isText.  If you do this, make sure that
+      // all assignments to isText use 0 or 1 (or True or False), and that a
+      // positive number larger than 1 is never used to represent True.
       UInt  size;   /* size in bytes */
       Bool  isText;
    }

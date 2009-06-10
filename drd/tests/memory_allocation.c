31fd94279e40d8b703afd525e9331b8df7e2e19b
Bart Van Assche <bvanassche@acm.org>
2009-06-10 17:51:52 +0000

    Added test code for realloc().
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@10292

--- drd/tests/memory_allocation.c
+++ drd/tests/memory_allocation.c
@@ -1,15 +1,28 @@
-/* Repeatedly allocate and free memory. Tests whether drd */
-/* really frees memory allocated by a client. See also    */
-/* http://bugs.kde.org/show_bug.cgi?id=161036             */
+/**
+ * @brief Repeatedly allocate and free memory. Tests whether drd really frees
+ *        memory allocated by a client. See also
+ *        http://bugs.kde.org/show_bug.cgi?id=161036.
+ */
 
+#include <assert.h>
 #include <stdlib.h>
 
 int main()
 {
   int i;
+  void* p;
+
+  for (i = 0; i < 100000; i++)
+    free(malloc(40960));
+
   for (i = 0; i < 100000; i++)
   {
-    free(malloc(40960));
+    p = realloc(NULL, 40960);
+    p = realloc(p, 50000);
+    p = realloc(p, 40000);
+    p = realloc(p, 0);
+    assert(! p);
   }
+
   return 0;
 }

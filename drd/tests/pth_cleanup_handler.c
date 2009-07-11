97dac310443dc926cb3e24b841f498d864f2ebe8
Bart Van Assche <bvanassche@acm.org>
2009-07-11 15:02:27 +0000

    pth_cleanup_handler test compiles and runs now on systems that do not support reader-writer locks too.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@10430

--- drd/tests/pth_cleanup_handler.c
+++ drd/tests/pth_cleanup_handler.c
@@ -11,20 +11,20 @@
 #include <stdlib.h>
 
 
-static pthread_rwlock_t rwl;
+static pthread_mutex_t s_mutex;
 
 
 static void cleanup_handler(void* param)
 {
   fprintf(stderr, "Cleanup handler has been called.\n");
-  pthread_rwlock_unlock(&rwl);
+  pthread_mutex_unlock(&s_mutex);
 }
 
 static void* f(void *p)
 {
-  if (pthread_rwlock_rdlock(&rwl) != 0)
+  if (pthread_mutex_lock(&s_mutex) != 0)
   {
-    fprintf(stderr, "pthread_rwlock_rdlock()\n");
+    fprintf(stderr, "pthread_mutex_lock()\n");
     exit(1);
   }
 
@@ -41,9 +41,9 @@ int main()
   // Make sure the program exits in case a deadlock has been triggered.
   alarm(2);
 
-  if (pthread_rwlock_init(&rwl, NULL) != 0)
+  if (pthread_mutex_init(&s_mutex, NULL) != 0)
   {
-    fprintf(stderr, "pthread_rwlock_init()\n");
+    fprintf(stderr, "pthread_mutex_init()\n");
     exit(1);
   }
   if (pthread_create(&pt1, NULL, f, NULL) != 0)

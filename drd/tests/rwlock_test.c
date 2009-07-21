76f381d299126b57bf5d1726c1cac87323fc7839
Bart Van Assche <bvanassche@acm.org>
2009-07-21 16:35:48 +0000

    Added more error checking.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@10511

--- drd/tests/rwlock_test.c
+++ drd/tests/rwlock_test.c
@@ -10,10 +10,29 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>  // strerror()
+
+
+#define PTH_CALL(expr)                                  \
+  do                                                    \
+  {                                                     \
+    int err = (expr);                                   \
+    if ((err) != 0)                                     \
+    {                                                   \
+      fprintf(stderr,                                   \
+              "%s:%d %s returned error code %d (%s)\n", \
+              __FILE__,                                 \
+              __LINE__,                                 \
+              #expr,                                    \
+              err,                                      \
+              strerror(err));                           \
+    }                                                   \
+  } while (0)
 
 
 static pthread_rwlock_t s_rwlock;
 static int s_counter;
+
 
 static void* thread_func(void* arg)
 {
@@ -22,12 +41,12 @@ static void* thread_func(void* arg)
 
   for (i = 0; i < 1000; i++)
   {
-    pthread_rwlock_rdlock(&s_rwlock);
+    PTH_CALL(pthread_rwlock_rdlock(&s_rwlock));
     sum += s_counter;
-    pthread_rwlock_unlock(&s_rwlock);
-    pthread_rwlock_wrlock(&s_rwlock);
+    PTH_CALL(pthread_rwlock_unlock(&s_rwlock));
+    PTH_CALL(pthread_rwlock_wrlock(&s_rwlock));
     s_counter++;
-    pthread_rwlock_unlock(&s_rwlock);
+    PTH_CALL(pthread_rwlock_unlock(&s_rwlock));
   }
 
   return 0;
@@ -39,15 +58,15 @@ int main(int argc, char** argv)
   pthread_t tid[thread_count];
   int i;
 
-  pthread_rwlock_init(&s_rwlock, NULL);
+  PTH_CALL(pthread_rwlock_init(&s_rwlock, NULL));
   for (i = 0; i < thread_count; i++)
   {
-    pthread_create(&tid[i], 0, thread_func, 0);
+    PTH_CALL(pthread_create(&tid[i], 0, thread_func, 0));
   }
 
   for (i = 0; i < thread_count; i++)
   {
-    pthread_join(tid[i], 0);
+    PTH_CALL(pthread_join(tid[i], 0));
   }
 
   fprintf(stderr, "Finished.\n");

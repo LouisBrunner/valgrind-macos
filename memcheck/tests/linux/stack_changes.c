ff3b5026123dacfe823e083ed5c54169b48c3778
Nicholas Nethercote <njn@valgrind.org>
2009-07-28 05:11:42 +0000

    Remove non-Linux code from this Linux-only test.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@10646

--- memcheck/tests/linux/stack_changes.c
+++ memcheck/tests/linux/stack_changes.c
@@ -10,11 +10,7 @@
 // This test is checking the libc context calls (setcontext, etc.) and
 // checks that Valgrind notices their stack changes properly.
 
-#if defined(_AIX) || defined(__APPLE__)
-typedef  ucontext_t  mycontext;
-#else /* linux */
 typedef  struct ucontext  mycontext;
-#endif
 
 mycontext ctx1, ctx2, oldc;
 int count;

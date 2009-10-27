20f6ef58968f9dde62f19a0a823001088f4d6358
Bart Van Assche <bvanassche@acm.org>
2009-10-27 14:20:23 +0000

    Added a modified version of the rt_sigqueue test program provided by Konstantin Serebryany.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@10910

---
+++ memcheck/tests/linux/sigqueue.c
@@ -0,0 +1,30 @@
+#include <signal.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <syscall.h>
+#include <unistd.h>
+
+int main()
+{
+  siginfo_t *si;
+  const size_t sz = sizeof(*si);
+
+  fprintf(stderr, "sizeof(*si) = %zu\n", sz);
+  fprintf(stderr, "%zd %zd %zd %zd\n",
+          offsetof(siginfo_t, si_signo),
+          offsetof(siginfo_t, si_errno),
+          offsetof(siginfo_t, si_code),
+          offsetof(siginfo_t, _sifields)
+         );
+  si = calloc(1, sz);
+  si->si_signo = SIGWINCH;
+  si->si_code = SI_QUEUE;
+  si->si_pid = getpid();
+  si->si_uid = getuid();
+  syscall(__NR_rt_sigqueueinfo, getpid(), SIGWINCH, si);
+  free(si);
+  fprintf(stderr, "Done.\n");
+  return 0;
+}

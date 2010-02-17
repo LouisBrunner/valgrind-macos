0e2536cf59e5172d28340facf0c2cc97860bd0b2
Julian Seward <jseward@acm.org>
2010-02-17 16:20:04 +0000

    Increase number of supported threads to 500.  Fixes #227328.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11049

--- include/pub_tool_threadstate.h
+++ include/pub_tool_threadstate.h
@@ -36,7 +36,7 @@
    scheduler algorithms is surely O(N) in the number of threads, since
    that's simple, at least.  And (in practice) we hope that most
    programs do not need many threads. */
-#define VG_N_THREADS 300
+#define VG_N_THREADS 500
 
 /* Special magic value for an invalid ThreadId.  It corresponds to
    LinuxThreads using zero as the initial value for

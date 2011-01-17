395293f5de2325dd5f3a83cb026bee936f21831d
Julian Seward <jseward@acm.org>
2011-01-17 11:15:48 +0000

    Add build system goop for testing SSE4.2 instructions.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11499

--- tests/x86_amd64_features.c
+++ tests/x86_amd64_features.c
@@ -92,6 +92,9 @@ static Bool go(char* cpu)
      level = 0x80000001;
      cmask = 1 << 5;
      require_amd = True;
+   } else if ( strcmp( cpu, "amd64-sse42" ) == 0 ) {
+     level = 1;
+     cmask = 1 << 20;
 #endif
    } else {
      return 2;          // Unrecognised feature.

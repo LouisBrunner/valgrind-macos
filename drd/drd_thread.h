2ca0b1b5d1c0a3e06e1ac1fc8d25760f58a31992
Bart Van Assche <bvanassche@acm.org>
2011-02-09 11:55:12 +0000

    DRD: handle fork() in processes with detached threads correctly (see also #255355).
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11532

--- drd/drd_thread.h
+++ drd/drd_thread.h
@@ -134,7 +134,7 @@ DrdThreadId DRD_(thread_pre_create)(const DrdThreadId creator,
                                     const ThreadId vg_created);
 DrdThreadId DRD_(thread_post_create)(const ThreadId vg_created);
 void DRD_(thread_post_join)(DrdThreadId drd_joiner, DrdThreadId drd_joinee);
-void DRD_(thread_delete)(const DrdThreadId tid);
+void DRD_(thread_delete)(const DrdThreadId tid, Bool detached);
 void DRD_(thread_finished)(const DrdThreadId tid);
 void DRD_(drd_thread_atfork_child)(const DrdThreadId tid);
 void DRD_(thread_pre_cancel)(const DrdThreadId tid);

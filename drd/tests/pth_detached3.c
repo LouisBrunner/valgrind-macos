c42807edf5cc3b8203016560983fb097de9a879d
Bart Van Assche <bvanassche@acm.org>
2011-03-09 17:50:34 +0000

    DRD: Added a regression test.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11613

---
+++ drd/tests/pth_detached3.c
@@ -0,0 +1,29 @@
+/* Invoke pthread_detach() with an invalid thread ID. */
+
+#include <assert.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+
+static void* thread_func(void* arg)
+{
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+  pthread_t thread;
+
+  pthread_create(&thread, NULL, thread_func, NULL);
+  pthread_join(thread, NULL);
+
+  /* Invoke pthread_detach() with the thread ID of a joined thread. */
+  pthread_detach(thread);
+
+  /* Invoke pthread_detach() with an invalid thread ID. */
+  pthread_detach(thread + 1);
+
+  fprintf(stderr, "Finished.\n");
+
+  return 0;
+}

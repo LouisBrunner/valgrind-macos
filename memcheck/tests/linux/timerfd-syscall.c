4a32a7d8be2b269cddb60d0346b4eb07e4b9bae0
Bart Van Assche <bvanassche@acm.org>
2011-03-17 07:45:46 +0000

    Yet another attempt to make the timerfd-syscall test reliable.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11647

--- memcheck/tests/linux/timerfd-syscall.c
+++ memcheck/tests/linux/timerfd-syscall.c
@@ -268,8 +268,8 @@ int main(int ac, char **av)
     else
     {
       const double delta = (ttmr - tnow) * 1e-6;
-      if (1.9 < delta && delta < 2.2)
-        fprintf(stderr, "got timer ticks (%ld) after about 2s\n", ticks);
+      if (19 <= ticks && ticks <= 21 && 1.9 < delta && delta < 2.2)
+        fprintf(stderr, "got about 20 timer ticks after about 2s\n", ticks);
       else
         fprintf(stderr, "got timer ticks (%ld) after %.2f s\n", ticks, delta);
     }

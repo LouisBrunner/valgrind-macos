b8a03bdc8e7c8f07405f92340d0e80e1bfe83a71
Julian Seward <jseward@acm.org>
2011-03-28 21:19:00 +0000

    Remove a bunch more warnings generated by gcc-4.6 about dead assignments ("[-Wunused-but-set-variable]"), in the test suite.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11675

--- callgrind/callgrind.h
+++ callgrind/callgrind.h
@@ -83,7 +83,7 @@ typedef
 
 /* Dump current state of cost centers, and zero them afterwards */
 #define CALLGRIND_DUMP_STATS                                            \
-   {unsigned int _qzz_res;                                              \
+   {unsigned int _qzz_res __attribute((unused));                        \
     VALGRIND_DO_CLIENT_REQUEST(_qzz_res, 0,                             \
                             VG_USERREQ__DUMP_STATS,                     \
                             0, 0, 0, 0, 0);                             \
@@ -94,7 +94,7 @@ typedef
    the dump. This string is written as a description field into the
    profile data dump. */
 #define CALLGRIND_DUMP_STATS_AT(pos_str)                                \
-   {unsigned int _qzz_res;                                              \
+   {unsigned int _qzz_res __attribute((unused));                        \
     VALGRIND_DO_CLIENT_REQUEST(_qzz_res, 0,                             \
                             VG_USERREQ__DUMP_STATS_AT,                  \
                             pos_str, 0, 0, 0, 0);                       \
@@ -102,7 +102,7 @@ typedef
 
 /* Zero cost centers */
 #define CALLGRIND_ZERO_STATS                                            \
-   {unsigned int _qzz_res;                                              \
+   {unsigned int _qzz_res __attribute((unused));                        \
     VALGRIND_DO_CLIENT_REQUEST(_qzz_res, 0,                             \
                             VG_USERREQ__ZERO_STATS,                     \
                             0, 0, 0, 0, 0);                             \
@@ -113,7 +113,7 @@ typedef
    should be noted or if they are to be ignored. Events are noted
    by increment of counters in a cost center */
 #define CALLGRIND_TOGGLE_COLLECT                                        \
-   {unsigned int _qzz_res;                                              \
+   {unsigned int _qzz_res __attribute((unused));                        \
     VALGRIND_DO_CLIENT_REQUEST(_qzz_res, 0,                             \
                             VG_USERREQ__TOGGLE_COLLECT,                 \
                             0, 0, 0, 0, 0);                             \
@@ -124,7 +124,7 @@ typedef
    this will lead to an artifical cache warmup phase afterwards with
    cache misses which would not have happened in reality. */
 #define CALLGRIND_START_INSTRUMENTATION                                 \
-   {unsigned int _qzz_res;                                              \
+   {unsigned int _qzz_res __attribute((unused));                        \
     VALGRIND_DO_CLIENT_REQUEST(_qzz_res, 0,                             \
                             VG_USERREQ__START_INSTRUMENTATION,          \
                             0, 0, 0, 0, 0);                             \
@@ -138,7 +138,7 @@ typedef
    To start Callgrind in this mode to ignore the setup phase, use
    the option "--instr-atstart=no". */
 #define CALLGRIND_STOP_INSTRUMENTATION                                  \
-   {unsigned int _qzz_res;                                              \
+   {unsigned int _qzz_res __attribute((unused));                        \
     VALGRIND_DO_CLIENT_REQUEST(_qzz_res, 0,                             \
                             VG_USERREQ__STOP_INSTRUMENTATION,           \
                             0, 0, 0, 0, 0);                             \

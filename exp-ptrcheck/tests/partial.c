d411ca1dee2c8aac1aadcb5f51951687716d3b41
Bart Van Assche <bvanassche@acm.org>
2011-04-04 10:15:33 +0000

    Made the line numbers in exp-ptrcheck/tests/partial.c and the regression test output match again.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11683

--- exp-ptrcheck/tests/partial.c
+++ exp-ptrcheck/tests/partial.c
@@ -3,16 +3,11 @@
 
 int main ( void )
 {
-   int* x3 = malloc(3);
-   float f __attribute__((unused)), *f3 = malloc(3);
-   int* x4 = malloc(4);
-   double d __attribute__((unused)), *d7 = malloc(7);
-   int* x5 = malloc(5);
-   long long int lli __attribute__((unused)), *lli7 = malloc(7);
-   int* x6 = malloc(6);
-   char c __attribute__((unused)), *c0   = malloc(0);
-   int* x7 = malloc(7);
-   short int s __attribute__((unused)), *s1 = malloc(1);
+   int* x3 = malloc(3); float f __attribute__((unused)), *f3 = malloc(3);
+   int* x4 = malloc(4); double d __attribute__((unused)), *d7 = malloc(7);
+   int* x5 = malloc(5); long long int lli __attribute__((unused)), *lli7 = malloc(7);
+   int* x6 = malloc(6); char c __attribute__((unused)), *c0   = malloc(0);
+   int* x7 = malloc(7); short int s __attribute__((unused)), *s1 = malloc(1);
    int  x __attribute__((unused));
    int* y4 = malloc(4);
    int* y5 = malloc(5);

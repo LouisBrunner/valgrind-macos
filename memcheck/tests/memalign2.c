c0bb2b82c6434092486c783ba4bc61fb29501f0d
Julian Seward <jseward@acm.org>
2011-09-10 10:17:35 +0000

    Avoid excessive fragmentation in m_mallocfree by munmapping unused superblocks in some circumstances (second attempt).  Bug 250101 comment 15.  (Philippe Waroquiers, philippe.waroquiers@skynet.be).
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@12022

--- memcheck/tests/memalign2.c
+++ memcheck/tests/memalign2.c
@@ -26,9 +26,37 @@ int main ( void )
 #  else
    // Nb: assuming VG_MIN_MALLOC_SZB is 8 or more...
    int* p;
+   int* piece;
    int  res;
    assert(sizeof(long int) == sizeof(void*));
 
+   // Check behaviour of memalign/free for big alignment.
+   // In particular, the below aims at checking that a
+   // superblock with a big size is not marked as reclaimable
+   // if the superblock is used to provide a big aligned block
+   // (see bug 250101, comment #14).
+   // Valgrind m_mallocfree.c will allocate a big superblock for the memalign
+   // call and will split it in two. This splitted superblock was
+   // wrongly marked as reclaimable, which was then causing
+   // assert failures (as reclaimable blocks cannot be splitted).
+   p = memalign(1024 * 1024, 4 * 1024 * 1024 + 1);   assert(0 == (long)p % (1024 * 1024));
+   // We allocate (and then free) a piece of memory smaller than
+   // the hole created in the big superblock.
+   // If the superblock is marked as reclaimable, the below free(s) will cause
+   // an assert. Note that the test has to be run with a --free-list-vol
+   // parameter smaller than the released blocks size to ensure the free is directly
+   // executed (otherwise memcheck does not really release the memory and so
+   // the bug is not properly tested).
+   piece = malloc(1024 * 1000); assert (piece);
+   free (piece);
+   free (p);
+   
+   // Same as above but do the free in the reverse order.
+   p = memalign(1024 * 1024, 4 * 1024 * 1024 + 1);   assert(0 == (long)p % (1024 * 1024));
+   piece = malloc(1024 * 100); assert (piece);
+   free (p);
+   free (piece);
+   
    p = memalign(0, 100);      assert(0 == (long)p % 8);
    p = memalign(1, 100);      assert(0 == (long)p % 8);
    p = memalign(2, 100);      assert(0 == (long)p % 8);
@@ -47,6 +75,7 @@ int main ( void )
    p = memalign(4095, 100);   assert(0 == (long)p % 4096);
    p = memalign(4096, 100);   assert(0 == (long)p % 4096);
    p = memalign(4097, 100);   assert(0 == (long)p % 8192);
+
 
 #  define PM(a,b,c) posix_memalign((void**)a, b, c)
 

3371d75b838b55bf5fdba24ed18f3ba277bd65d2
Bart Van Assche <bvanassche@acm.org>
2011-10-22 14:38:47 +0000

    Revert r12195 because it triggers suspicious error messages on systems where libtool has not been installed:
    Can't exec "libtoolize": No such file or directory at /usr/bin/autoreconf line 196.
    Use of uninitialized value in pattern match (m//) at /usr/bin/autoreconf line 196.
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@12198

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-autoreconf -ivf
+run ()
+{
+    echo "running: $*"
+    eval $*
+
+    if test $? != 0 ; then
+	echo "error: while running '$*'"
+	exit 1
+    fi
+}
+
+run aclocal
+run autoheader
+run automake -a
+run autoconf

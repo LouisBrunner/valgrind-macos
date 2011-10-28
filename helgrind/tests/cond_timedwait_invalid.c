b84e2153431f1ce3542dae032499b2aa4263473b
Florian Krohm <florian@eich-krohm.de>
2011-10-28 00:11:44 +0000

    Rewrite test to not use function clock_gettime. The purpose of this is to not have to link against -lrt because that causes a different back-trace on certain x86 and s390x environments. See the thread with subject "helgrind/tests/cond_timedwait_invalid failing on x86" on valgrind-developers for more details.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@12246

--- helgrind/tests/cond_timedwait_invalid.c
+++ helgrind/tests/cond_timedwait_invalid.c
@@ -1,4 +1,4 @@
-#include "config.h"
+
 #include <time.h>
 #include <pthread.h>
 #include <assert.h>
@@ -10,12 +10,12 @@ int main()
    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-#ifdef HAVE_CLOCK_GETTIME
-   assert(clock_gettime(CLOCK_REALTIME, &abstime)==0);
-#else
+
+
+
    abstime.tv_sec = time(NULL) + 2;
    abstime.tv_nsec = 0;
-#endif
+
    abstime.tv_nsec += 1000000000;
    
    assert(pthread_mutex_lock(&mutex)==0);

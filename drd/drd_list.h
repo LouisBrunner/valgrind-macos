75a946033a5f34783c002c4514ee50d693632ada
Bart Van Assche <bvanassche@acm.org>
2012-01-25 11:05:12 +0000

    Fix a copy/paste error
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@12355

--- drd/drd_list.h
+++ drd/drd_list.h
@@ -5,9 +5,8 @@
   Copyright (C) 2012 Bart Van Assche <bvanassche@acm.org>.
 
   This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License as
-  published by the Free Software Foundation; either version 2 of the
-  License, or (at your option) any later version.
+  modify it under the terms of version 2 of the GNU General Public
+  License as published by the Free Software Foundation.
 
   This program is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of

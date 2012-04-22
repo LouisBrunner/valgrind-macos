ac933b157cc7475119cd4c65a87b8d7983f10a92
Florian Krohm <florian@eich-krohm.de>
2012-04-22 03:48:52 +0000

    Rename to VEX_S390X_MODEL_UNKNOWN.
    git-svn-id: svn://svn.valgrind.org/vex/trunk@2307

--- VEX/pub/libvex.h
+++ VEX/pub/libvex.h
@@ -123,7 +123,7 @@ typedef
 #define VEX_S390X_MODEL_Z10_BC   7
 #define VEX_S390X_MODEL_Z196     8
 #define VEX_S390X_MODEL_Z114     9
-#define VEX_S390X_MODEL_INVALID  10
+#define VEX_S390X_MODEL_UNKNOWN  10     /* always last in list */
 #define VEX_S390X_MODEL_MASK     0x3F
 
 #define VEX_HWCAPS_S390X_LDISP (1<<6)   /* Long-displacement facility */

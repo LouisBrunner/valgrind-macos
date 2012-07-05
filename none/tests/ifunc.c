1a6f7a03710f717463850f9812cfa47c28eddf44
Florian Krohm <florian@eich-krohm.de>
2012-07-05 21:21:37 +0000

    Add testcase from bugzilla #301204. Testcase by Chantry Xavier (shiningxc@gmail.com).
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@12715

---
+++ none/tests/ifunc.c
@@ -0,0 +1,21 @@
+/* This test made valgrind run in an infinite loop. See bugzilla #301204 */
+#include <stdio.h>
+
+static void mytest(int d)
+{
+    printf("%d\n", d);
+}
+
+static void (*resolve_test(void))(void)
+{
+    return (void (*)(void))&mytest;
+}
+
+void test(int d)
+    __attribute__((ifunc("resolve_test")));
+
+int main()
+{
+    test(5);
+    return 0;
+}

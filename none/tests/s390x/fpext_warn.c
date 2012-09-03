2d935572f52b4f6645ca41a0bff4017058f434d0
Florian Krohm <florian@eich-krohm.de>
2012-09-03 17:45:15 +0000

    Update testcase as a followup to VEX r2509.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@12955

--- none/tests/s390x/fpext_warn.c
+++ none/tests/s390x/fpext_warn.c
@@ -8,10 +8,10 @@ int
 main(void)
 {
    printf("before\n");
-   __asm__ volatile ( CFEBRA(5,0,0,0) : : : "cc", "memory");
+   __asm__ volatile ( CEGBRA(1,0,0,0) : : : "cc", "memory");
+   __asm__ volatile ( CEFBRA(3,0,0,0) : : : "cc", "memory");
+   __asm__ volatile ( CDGBRA(4,0,0,0) : : : "cc", "memory");
    __asm__ volatile ( CEFBRA(5,0,0,0) : : : "cc", "memory");
-   __asm__ volatile ( CXFBRA(5,0,0,0) : : : "cc", "memory");
-   __asm__ volatile ( CGXBRA(5,0,0,0) : : : "cc", "memory");
 
    printf("after\n");
    return 0;

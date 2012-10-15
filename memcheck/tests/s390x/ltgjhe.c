37009382a6f9179a7bab69b420daff90b8256012
Christian Borntraeger <borntraeger@de.ibm.com>
2012-10-15 14:01:31 +0000

    Add testcase for checking the ltg + jhe handling
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@13044

---
+++ memcheck/tests/s390x/ltgjhe.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+	int field1, field2;
+	int result;
+
+	/*
+         * gcc does some tricks for checking the highest bit. It seems
+         * to load a full word/double word. 
+         * By using mask=10 for brc (jhe) only the msb is influencing
+         * the code flow. This test was inspired by 308427
+         */
+	asm volatile(	"oi %1,128\n\t"
+			"ltg 0,%1\n\t"
+			"jhe 1f\n\t"
+			"lghi %0,0\n\t"
+			"j 2f\n\t"
+			"1:lghi %0,0\n\t"
+			"2:\n\t"
+			:"=d" (result)
+			:"Q" (field1)
+			:"0","cc");
+
+	if (result)
+		printf("Error\n");
+
+	asm volatile(	"oi %1,128\n\t"
+			"lt 0,%1\n\t"
+			"jhe 1f\n\t"
+			"lghi %0,0\n\t"
+			"j 2f\n\t"
+			"1:lghi %0,0\n\t"
+			"2:\n\t"
+			:"=d" (result)
+			:"Q" (field2)
+			:"0","cc");
+
+	if (result)
+		printf("Error\n");
+
+	return 0;
+}

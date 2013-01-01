963e82c6fe66919c84c4492c0b3bd03170bcac31
Florian Krohm <florian@eich-krohm.de>
2013-01-01 22:20:12 +0000

    s390: Add testcases for DFP "shift significant" insns. Patch by Maran Pakkirisamy (maranp@linux.vnet.ibm.com). Part of fixing BZ 307113.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@13215

--- none/tests/s390x/dfp-2.c
+++ none/tests/s390x/dfp-2.c
@@ -54,6 +54,70 @@ void ltxtr(_Decimal128 in)
   printf(" -> %d\n", cc);
 }
 
+void sldt(_Decimal64 in, unsigned long amount)
+{
+  _Decimal64 out;
+  int *shift = (int *) amount;
+
+  asm volatile (".insn rxf, 0xed0000000040, %[out], %[in], 0(%[amount])\n\t"
+                :[out]"=f"(out)
+                :[in]"f"(in),[amount]"a"(shift));
+
+  printf("SLDT ");
+  DFP_VAL_PRINT(in, _Decimal64);
+  printf(" -> ");
+  DFP_VAL_PRINT(out, _Decimal64);
+  printf("\n");
+}
+
+void slxt(_Decimal128 in, unsigned long amount)
+{
+  _Decimal128 out;
+  int *shift = (int *) amount;
+
+  asm volatile (".insn rxf, 0xed0000000048, %[out], %[in], 0(%[amount])\n\t"
+                :[out]"=f"(out)
+                :[in]"f"(in),[amount]"a"(shift));
+
+  printf("SLXT ");
+  DFP_VAL_PRINT(in, _Decimal128);
+  printf(" -> ");
+  DFP_VAL_PRINT(out, _Decimal128);
+  printf("\n");
+}
+
+void srdt(_Decimal64 in, unsigned long amount)
+{
+  _Decimal64 out;
+  int *shift = (int *) amount;
+
+  asm volatile (".insn rxf, 0xed0000000041, %[out], %[in], 0(%[amount])\n\t"
+                :[out]"=f"(out)
+                :[in]"f"(in),[amount]"a"(shift));
+
+  printf("SRDT ");
+  DFP_VAL_PRINT(in, _Decimal64);
+  printf(" -> ");
+  DFP_VAL_PRINT(out, _Decimal64);
+  printf("\n");
+}
+
+void srxt(_Decimal128 in, unsigned long amount)
+{
+  _Decimal128 out;
+  int *shift = (int *) amount;
+
+  asm volatile (".insn rxf, 0xed0000000049, %[out], %[in], 0(%[amount])\n\t"
+                :[out]"=f"(out)
+                :[in]"f"(in),[amount]"a"(shift));
+
+  printf("SRXT ");
+  DFP_VAL_PRINT(in, _Decimal128);
+  printf(" -> ");
+  DFP_VAL_PRINT(out, _Decimal128);
+  printf("\n");
+}
+
 int main() {
   _Decimal64 d64 = 50.0005DD;
   _Decimal128 d128 = 50.0005DL;
@@ -71,6 +135,28 @@ int main() {
   ltxtr(d128);
   ltxtr(-d128);
   ltxtr(0.0DL);
+
+  d64 = 12345678.54321DD;
+  sldt(d64, 10);
+  sldt(-d64, 2);
+  sldt(0.DD, 2);
+  sldt(-0.DD, 2);
+
+  srdt(d64, 5);
+  srdt(-d64, 2);
+  srdt(0.DD, 2);
+  srdt(-0.DD, 2);
+
+  d128 = 12345678.54321DL;
+  slxt(d128, 10);
+  slxt(-d128, 2);
+  slxt(0.DL, 2);
+  slxt(-0.DL, 2);
+
+  srxt(d128, 10);
+  srxt(-d128, 2);
+  srxt(0.DL, 2);
+  srxt(-0.DL, 2);
 
   return 0;
 }

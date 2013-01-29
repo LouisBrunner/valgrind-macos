d120d46f76fbc8b31f957144adc4a5aeb677530e
Julian Seward <jseward@acm.org>
2013-01-29 21:13:00 +0000

    get_otrack_shadow_offset_wrk: amd64: add a (very rare) missing case.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@13277

--- memcheck/mc_machine.c
+++ memcheck/mc_machine.c
@@ -593,6 +593,7 @@ static Int get_otrack_shadow_offset_wrk ( Int offset, Int szB )
    if (o == GOF(GS_0x60) && sz == 8) return -1; /* slot unused */
    if (o == GOF(TISTART) && sz == 8) return -1; /* slot unused */
    if (o == GOF(TILEN)   && sz == 8) return -1; /* slot unused */
+   if (o == GOF(NRADDR)  && sz == 8) return -1; /* slot unused */
 
    /* Treat %AH, %BH, %CH, %DH as independent registers.  To do this
       requires finding 4 unused 32-bit slots in the second-shadow

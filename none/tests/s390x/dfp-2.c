1f53bd2199354b46b2be22d36ac372c8c72c513a
Florian Krohm <florian@eich-krohm.de>
2013-02-14 14:28:22 +0000

    s390: Testcases and vbit-tester changes for the following DFP insns: - extract basied exponent - insert biased exponent - quantize - reround to significance Patch by Maran Pakkirisamy (maranp@linux.vnet.ibm.com). Part of fixing BZ #307113.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@13286

--- none/tests/s390x/dfp-2.c
+++ none/tests/s390x/dfp-2.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "dfp_utils.h"
 
 /* Test various DFP ops:
+   - extract biased exponent 64/128 bit
    - extract significance 64/128 bit
+   - insert biased exponent 64/128 bit
    - load and test 64/128 bit
+   - shift left/right 64/128 bit
+   - reround 64/128 bit
 */
+
+void eedtr(_Decimal64 in)
+{
+  long out;
+  asm volatile(".insn rre, 0xb3e50000, %[out], %[in]\n\t"
+               :[out] "=d" (out) :[in] "f" (in));
+  printf("EEDTR ");
+  DFP_VAL_PRINT(in, _Decimal64);
+  printf(" -> %ld\n", out);
+}
+
+void eextr(_Decimal128 in)
+{
+  long out;
+  asm volatile(".insn rre, 0xb3ed0000, %[out], %[in]\n\t"
+               :[out] "=d" (out) :[in] "f" (in));
+  printf("EEXTR ");
+  DFP_VAL_PRINT(in, _Decimal128);
+  printf(" -> %ld\n", out);
+}
 
 void esdtr(_Decimal64 in)
 {
@@ -24,6 +49,36 @@ void esxtr(_Decimal128 in)
   printf("ESXTR ");
   DFP_VAL_PRINT(in, _Decimal128);
   printf(" -> %ld\n", out);
+}
+
+void iedtr(_Decimal64 in, long amount)
+{
+  _Decimal64 out;
+
+  asm volatile (".insn rrf, 0xb3f60000, %[out], %[amount], %[in], 0\n\t"
+                :[out]"=f"(out)
+                :[in]"f"(in), [amount]"d"(amount));
+
+  printf("IEDTR ");
+  DFP_VAL_PRINT(in, _Decimal64);
+  printf(", %ld -> ", amount);
+  DFP_VAL_PRINT(out, _Decimal64);
+  printf("\n");
+}
+
+void iextr(_Decimal128 in, long amount)
+{
+  _Decimal128 out;
+
+  asm volatile (".insn rrf, 0xb3fe0000, %[out], %[amount], %[in], 0\n\t"
+                :[out]"=f"(out)
+                :[in]"f"(in), [amount]"d"(amount));
+
+  printf("IEXTR ");
+  DFP_VAL_PRINT(in, _Decimal128);
+  printf(", %ld -> ", amount);
+  DFP_VAL_PRINT(out, _Decimal128);
+  printf("\n");
 }
 
 void ltdtr(_Decimal64 in)
@@ -52,6 +107,106 @@ void ltxtr(_Decimal128 in)
   printf("LTXTR ");
   DFP_VAL_PRINT(in, _Decimal128);
   printf(" -> %d\n", cc);
+}
+
+void qadtr(_Decimal64 op, _Decimal64 quan, uint8_t rm)
+{
+  _Decimal64 out;
+
+  asm volatile (
+                ".insn rrf, 0xb3f50000, %[out], %[quan], %[op], %[rm]\n\t"
+                :[out]"=f"(out)
+                :[op]"f"(op), [quan]"f"(quan), [rm]"d"(rm)
+                );
+  printf("QADTR ");
+  DFP_VAL_PRINT(op, _Decimal64);
+  printf(", ");
+  DFP_VAL_PRINT(quan, _Decimal64);
+  printf(", %x -> ", rm);
+  DFP_VAL_PRINT(out, _Decimal64);
+  printf("\n");
+}
+
+void quantize64(_Decimal64 op, _Decimal64 quan)
+{
+  uint8_t i;
+
+  for (i = 0; i < 16; i++)
+    qadtr(op, quan, i);
+}
+
+void qaxtr(_Decimal128 op, _Decimal128 quan, uint8_t rm)
+{
+  _Decimal128 out;
+
+  asm volatile (
+                ".insn rrf, 0xb3fd0000, %[out], %[quan], %[op], %[rm]\n\t"
+                :[out]"=f"(out)
+                :[op]"f"(op), [quan]"f"(quan), [rm]"d"(rm)
+                );
+  printf("QAXTR ");
+  DFP_VAL_PRINT(op, _Decimal128);
+  printf(", ");
+  DFP_VAL_PRINT(quan, _Decimal128);
+  printf(", %x -> ", rm);
+  DFP_VAL_PRINT(out, _Decimal128);
+  printf("\n");
+}
+
+void quantize128(_Decimal128 op, _Decimal128 quan)
+{
+  uint8_t i;
+
+  for (i = 0; i < 16; i++)
+    qaxtr(op, quan, i);
+}
+
+void rrdtr(_Decimal64 op, uint8_t sig, uint8_t rm)
+{
+  _Decimal64 out;
+
+  asm volatile (
+                ".insn rrf, 0xb3f70000, %[out], %[sig], %[op], %[rm]\n\t"
+                :[out]"=f"(out)
+                :[op]"f"(op), [sig]"d"(sig), [rm]"d"(rm)
+                );
+  printf("RRDTR ");
+  DFP_VAL_PRINT(op, _Decimal64);
+  printf(", %d, %x -> ", sig, rm);
+  DFP_VAL_PRINT(out, _Decimal64);
+  printf("\n");
+}
+
+void reround64(_Decimal64 op, uint8_t sig)
+{
+  uint8_t i;
+
+  for (i = 0; i < 16; i++)
+    rrdtr(op, sig, i);
+}
+
+void rrxtr(_Decimal128 op, uint8_t sig, uint8_t rm)
+{
+  _Decimal128 out;
+
+  asm volatile (
+                ".insn rrf, 0xb3ff0000, %[out], %[sig], %[op], %[rm]\n\t"
+                :[out]"=f"(out)
+                :[op]"f"(op), [sig]"d"(sig), [rm]"d"(rm)
+                );
+  printf("RRXTR ");
+  DFP_VAL_PRINT(op, _Decimal128);
+  printf(", %d, %x -> ", sig, rm);
+  DFP_VAL_PRINT(out, _Decimal128);
+  printf("\n");
+}
+
+void reround128(_Decimal128 op, uint8_t sig)
+{
+  uint8_t i;
+
+  for (i = 0; i < 16; i++)
+    rrxtr(op, sig, i);
 }
 
 void sldt(_Decimal64 in, unsigned long amount)
@@ -122,6 +277,13 @@ int main() {
   _Decimal64 d64 = 50.0005DD;
   _Decimal128 d128 = 50.0005DL;
 
+  eedtr(d64);
+  eedtr(-d64);
+  eedtr(0.DD);
+  eextr(d128);
+  eextr(-d128);
+  eextr(0.DL);
+
   esdtr(d64);
   esdtr(-d64);
   esdtr(0.DD);
@@ -157,6 +319,58 @@ int main() {
   srxt(-d128, 2);
   srxt(0.DL, 2);
   srxt(-0.DL, 2);
+
+  d64 = 5.000005DD;
+  iedtr(d64, 391);
+  iedtr(d64, 392);
+  iedtr(d64, 393);
+  iedtr(-d64, 391);
+  iedtr(-d64, 392);
+  iedtr(-d64, 393);
+  iedtr(0.DD, 393);
+  iedtr(-0.DD, 393);
+  iedtr(1.DD, 393);
+
+  d128 = 5.000005DL;
+  iextr(d128, 6169);
+  iextr(d128, 6170);
+  iextr(d128, 6171);
+  iextr(-d128, 6169);
+  iextr(-d128, 6170);
+  iextr(-d128, 6171);
+  iextr(0.DL, 6171);
+  iextr(-0.DL, 6171);
+  iextr(1.DL, 6171);
+
+  d64 = 2.171234DD;
+  quantize64(d64, 0.001DD);
+  quantize64(-d64, 0.001DD);
+  quantize64(-d64, 0.DD);
+  quantize64(0.DD, 0.001DD);
+
+  d128 = 26365343648.171234DL;
+  quantize128(d128, 230.01DL);
+  quantize128(-d128, 230.01DL);
+  quantize128(d128, 0.DL);
+  quantize128(-0.DL, 230.01DL);
+
+  d64 = 2.174598DD;
+  reround64(d64, 3);
+  reround64(d64, 4);
+  reround64(d64, 5);
+  reround64(-d64, 3);
+  reround64(-d64, 4);
+  reround64(-d64, 5);
+  reround64(0.DD, 0);
+
+  d128 = 2.174598DL;
+  reround128(d128, 3);
+  reround128(d128, 4);
+  reround128(d128, 5);
+  reround128(-d128, 3);
+  reround128(-d128, 4);
+  reround128(-d128, 5);
+  reround128(0.DL, 0);
 
   return 0;
 }

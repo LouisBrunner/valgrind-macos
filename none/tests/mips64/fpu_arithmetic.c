eb65531207dfd51bd80bd5b86878f7ae70f73e95
Dejan Jevtic <dejan.jevtic@valgrind.org>
2013-10-10 09:10:35 +0000

    mips64: Change the input values for FPU tests.
    Change the input values so that the binary values
    is representing exactly the same float values.
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@13632

--- none/tests/mips64/fpu_arithmetic.c
+++ none/tests/mips64/fpu_arithmetic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "rounding_mode.h"
 #include "macro_fpu.h"
 
@@ -16,127 +17,134 @@ int arithmeticOperations(flt_art_op_t op)
          switch(op) {
             case ABSS:
                UNOPff("abs.s");
-               printf("%s %.6g %f\n", flt_art_op_names[op], fd_f, fs_f[i]);
+               printf("%s %f %f\n", flt_art_op_names[op], fd_f, fs_f[i]);
                break;
             case ABSD:
                UNOPdd("abs.d");
-               printf("%s %.13g %lf\n", flt_art_op_names[op], fd_d, fs_d[i]);
+               printf("%s %lf %lf\n", flt_art_op_names[op], fd_d, fs_d[i]);
                break;
             case ADDS:
                BINOPf("add.s");
-               printf("%s %.6g %f %f\n",
+               printf("%s %f %f %f\n",
                       flt_art_op_names[op], fd_f, fs_f[i], ft_f[i]);
                break;
             case ADDD:
                BINOPd("add.d");
-               printf("%s %.13g %lf %lf\n",
+               printf("%s %lf %lf %lf\n",
                       flt_art_op_names[op], fd_d, fs_d[i], ft_d[i]);
                break;
             case DIVS:
                BINOPf("div.s");
-               printf("%s %.6g %f %f\n",
-                      flt_art_op_names[op], fd_f, fs_f[i], ft_f[i]);
+               printf("%s %f %f %f\n",
+                      flt_art_op_names[op], roundf(fd_f), fs_f[i], ft_f[i]);
                break;
             case DIVD:
                BINOPd("div.d");
-               printf("%s %.13g %lf %lf\n",
-                      flt_art_op_names[op], fd_d, fs_d[i], ft_d[i]);
+               printf("%s %lf %lf %lf\n",
+                      flt_art_op_names[op], round(fd_d), fs_d[i], ft_d[i]);
                break;
             case MULS:
                BINOPf("mul.s");
-               printf("%s %.6g %f %f\n",
-                      flt_art_op_names[op], fd_f, fs_f[i], ft_f[i]);
+               printf("%s %f %f %f\n",
+                      flt_art_op_names[op], roundf(fd_f), fs_f[i], ft_f[i]);
                break;
             case MULD:
                BINOPd("mul.d");
-               printf("%s %.13g %lf %lf\n",
-                      flt_art_op_names[op], fd_d, fs_d[i], ft_d[i]);
+               printf("%s %lf %lf %lf\n",
+                      flt_art_op_names[op], round(fd_d), fs_d[i], ft_d[i]);
                break;
             case NEGS:
                UNOPff("neg.s");
-               printf("%s %.6g %f\n", flt_art_op_names[op], fd_f, fs_f[i]);
+               printf("%s %f %f\n", flt_art_op_names[op], fd_f, fs_f[i]);
                break;
             case NEGD:
                UNOPdd("neg.d");
-               printf("%s %.13g %lf\n", flt_art_op_names[op], fd_d, fs_d[i]);
+               printf("%s %lf %lf\n", flt_art_op_names[op], fd_d, fs_d[i]);
                break;
             case SQRTS:
                UNOPff("sqrt.s");
-               printf("%s %.6g %f\n", flt_art_op_names[op], fd_f, fs_f[i]);
+               printf("%s %f %f\n",
+                      flt_art_op_names[op], roundf(fd_f), fs_f[i]);
                break;
             case SQRTD:
                UNOPdd("sqrt.d");
-               printf("%s %.13g %lf\n", flt_art_op_names[op], fd_d, fs_d[i]);
+               printf("%s %lf %lf\n",
+                      flt_art_op_names[op], round(fd_d), fs_d[i]);
                break;
             case SUBS:
                BINOPf("sub.s");
-               printf("%s %.6g %f %f\n",
+               printf("%s %f %f %f\n",
                       flt_art_op_names[op], fd_f, fs_f[i], ft_f[i]);
                break;
             case SUBD:
                BINOPd("sub.d");
-               printf("%s %.13g %lf %lf\n",
+               printf("%s %lf %lf %lf\n",
                       flt_art_op_names[op], fd_d, fs_d[i], ft_d[i]);
                break;
             case RECIPS:
                UNOPff("recip.s");
-               printf("%s %.6g %f\n", flt_art_op_names[op], fd_f, fs_f[i]);
+               printf("%s %f %f\n",
+                      flt_art_op_names[op], roundf(fd_f), fs_f[i]);
                break;
             case RECIPD:
                UNOPdd("recip.d");
-               printf("%s %.13g %lf\n", flt_art_op_names[op], fd_d, fs_d[i]);
+               printf("%s %lf %lf\n",
+                      flt_art_op_names[op], round(fd_d), fs_d[i]);
                break;
             case RSQRTS:
                if (fs_f[i] >= 0) {
                   UNOPff("rsqrt.s");
-                  printf("%s %.6g %f\n", flt_art_op_names[op], fd_f, fs_f[i]);
+                  printf("%s %f %f\n",
+                         flt_art_op_names[op], roundf(fd_f), fs_f[i]);
                }
                break;
             case RSQRTD:
                if (fs_d[i] >= 0) {
                   UNOPdd("rsqrt.d");
-                  printf("%s %.13g %lf\n", flt_art_op_names[op], fd_d, fs_d[i]);
+                  printf("%s %lf %lf\n",
+                         flt_art_op_names[op], round(fd_d), fs_d[i]);
                }
                break;
             case MSUBS:
                TRIOPf("msub.s");
-               printf("%s %.6g %f %f %f\n",
-                      flt_art_op_names[op], fd_f, fr_f[i], fs_f[i], ft_f[i]);
+               printf("%s %f %f %f %f\n",flt_art_op_names[op], roundf(fd_f),
+                                         fr_f[i], fs_f[i], ft_f[i]);
                break;
             case MSUBD:
                TRIOPd("msub.d");
-               printf("%s %.13g %lf %lf %lf\n",
-                      flt_art_op_names[op], fd_d, fr_d[i], fs_d[i], ft_d[i]);
+               printf("%s %lf %lf %lf %lf\n", flt_art_op_names[op], round(fd_d),
+                                              fr_d[i], fs_d[i], ft_d[i]);
                break;
             case MADDS:
                TRIOPf("madd.s");
-               printf("%s %.6g %f %f %f\n",
-                      flt_art_op_names[op], fd_f, fr_f[i], fs_f[i], ft_f[i]);
+               printf("%s %f %f %f %f\n", flt_art_op_names[op], roundf(fd_f),
+                                          fr_f[i], fs_f[i], ft_f[i]);
                break;
             case MADDD:
                TRIOPd("madd.d");
-               printf("%s %.13g %lf %lf %lf\n",
-                      flt_art_op_names[op], fd_d, fr_d[i], fs_d[i], ft_d[i]);
+               printf("%s %lf %lf %lf %lf\n", flt_art_op_names[op], round(fd_d),
+                                              fr_d[i], fs_d[i], ft_d[i]);
                break;
             case NMADDS:
                TRIOPf("nmadd.s");
-               printf("%s %.6g %f %f %f\n",
-                      flt_art_op_names[op], fd_f, fr_f[i], fs_f[i], ft_f[i]);
+               printf("%s %f %f %f %f\n", flt_art_op_names[op], roundf(fd_f),
+                                          fr_f[i], fs_f[i], ft_f[i]);
                break;
             case NMADDD:
                TRIOPd("nmadd.d");
-               printf("%s %.13g %lf %lf %lf\n",
-                      flt_art_op_names[op], fd_d, fr_d[i], fs_d[i], ft_d[i]);
+               printf("%s %lf %lf %lf %lf\n", flt_art_op_names[op], round(fd_d),
+                                              fr_d[i], fs_d[i], ft_d[i]);
                break;
             case NMSUBS:
                TRIOPf("nmsub.s");
-               printf("%s %.6g %f %f %f\n",
-                      flt_art_op_names[op], fd_f, fr_f[i], fs_f[i], ft_f[i]);
+               printf("%s %f %f %f %f\n", flt_art_op_names[op], roundf(fd_f),
+                                          fr_f[i], fs_f[i], ft_f[i]);
                break;
             case NMSUBD:
                TRIOPd("nmsub.d");
-               printf("%s %.13g %lf %lf %lf\n",
-                      flt_art_op_names[op], fd_d, fr_d[i], fs_d[i], ft_d[i]);
+               printf("%s 0x%lf %lf %lf %lf\n", flt_art_op_names[op],
+                                                round(fd_d), fr_d[i], fs_d[i],
+                                                ft_d[i]);
                break;
             default:
                printf("error\n");

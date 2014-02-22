5428b4f7a264c46b7481475df36304b49d3fb337
Florian Krohm <florian@eich-krohm.de>
2014-02-22 15:26:30 +0000

    Bump __VALGRIND_MINOR__  to catch up with the release.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@13831

--- include/valgrind.h
+++ include/valgrind.h
@@ -89,7 +89,7 @@
         || (__VALGRIND_MAJOR__ == 3 && __VALGRIND_MINOR__ >= 6))
 */
 #define __VALGRIND_MAJOR__    3
-#define __VALGRIND_MINOR__    8
+#define __VALGRIND_MINOR__    9
 
 
 #include <stdarg.h>

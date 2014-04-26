ec8e1458503447c9917de99e92c2c1135edee4c6
Philippe Waroquiers <philippe.waroquiers@skynet.be>
2014-04-26 21:50:57 +0000

    Add a new test that shows a surprising side effect of the medium resolution (4 callers) used to compare errors.
    To look at the strange side effect, do:
      ./vg-in-place -v --suppressions=memcheck/tests/suppfreecollision.supp memcheck/tests/suppfree activatenondangerouserror
    
    You obtain at the end:
    ...
    --19240-- used_suppression:      2 suppressnondangerouserror memcheck/tests/suppfreecollision.supp:2
    ...
    
    showing that the suppression aiming at suppressing a nondangerous error has in fact
    suppressed more than expected.
    
    This is because m_errormgr.c compares the exe_context in medium resolution/4 calls
    (or low resolution/2 calls  once 100 errors have been collected).
    
    The error machinery first encounters the non dangerous error. This error is suppressed,
    because all callers match the suppression entry. In particular, we have
    in the stacktrace the function ok_to_suppress_double_free_from_this_fun
    
    Then the error machinery encounters the second error.
    The stacktrace of the 2nd error has the same first 4 callers than the non
    dangerous error. So the 2nd error is considered equal to the first one
    and is (unexpectedly in my opinion) suppressed.
    
    This looks a bug (or at least something very surprising).
    (the doc mentions the fact that errors are 'commoned up' on 4 callers, but
    I am not sure the above side effect was understood).
    
    
    There are several ways this can be improved, some are more easier than other
      * have   --error-resolution=low/med/high
        similar to the memcheck   --leak-resolution=low/med/high
         (which default value would we take for this new clo ?)
    
      * have a lot more intelligent error comparison:
          when comparing an error with a suppressed error, one must
          check that the callers used for suppression are equal.
         This looks difficult to implement and probably a significant slow down
         in the error machinery, which will impact applications producing
         many suppressed errors (e.g. helgrind + some pthread lib errors).
         This also implies more memory (e.g. one byte per caller in the
         error, to indicate which caller(s) were used to suppress.
         Still wondering what to do with * and ... ?
    
      * have a somewhat more intelligent error comparison:
         Instead of comparing only the callers used for suppression, we
         compare the range first..last caller used (so including some
         callers in the range that were not used to suppressed if e.g.
         a ... matching was put in the supp entry).
         Probably still a slowdown (less than previous solution ?)
         and less memory than the previous solution.
         But also not completely clear how to compute the range.
    
      * always re-evaluate the suppression : this will very probably be
        a significant slow down.
    
      * do nothing, as nobody complained about this behaviour up to now :)
    
      * ??? any other idea
    
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@13914

--- memcheck/tests/suppfree.c
+++ memcheck/tests/suppfree.c
@@ -22,9 +22,17 @@ void aaa (char* x)
   bbb(x);
 }
 
-int main ( void )
+void ok_to_suppress_double_free_from_this_fun(char* y)
+{
+  aaa(y);
+}
+
+int main ( int argc, char*argv[] )
 {
    char* x = malloc(10);
+   char* y = malloc(10);
+   if (argc > 1)
+      ok_to_suppress_double_free_from_this_fun(y);
    aaa(x);
    return 0;
 }

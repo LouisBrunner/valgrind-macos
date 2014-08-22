6962af41ae1ccad1d14ffb1a010db0dc99e48fec
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2014-08-22 08:38:40 +0000

    Align header comment with other files
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@14330

--- callgrind/costs.h
+++ callgrind/costs.h
@@ -1,11 +1,35 @@
 /*--------------------------------------------------------------------*/
-/*--- Callgrind                                                    ---*/
-/*---                                                   ct_costs.h ---*/
-/*--- (C) 2004, Josef Weidendorfer                                 ---*/
+/*--- Callgrind cost array interface.                      costs.h ---*/
 /*--------------------------------------------------------------------*/
 
-#ifndef CT_COSTS
-#define CT_COSTS
+/*
+   This file is part of Valgrind, a dynamic binary instrumentation
+   framework.
+
+   Copyright (C) 2004-2014 Josef Weidendorfer
+      josef.weidendorfer@gmx.de
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307, USA.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
+
+#ifndef CLG_COSTS
+#define CLG_COSTS
 
 #include "pub_tool_basics.h"
 
@@ -32,4 +56,4 @@ ULong* CLG_(get_costarray)(Int size);
 void CLG_(free_costarrays)(void);
 
 
-#endif /* CT_COSTS */
+#endif /* CLG_COSTS */
--- callgrind/global.h
+++ callgrind/global.h
@@ -1,8 +1,31 @@
 /*--------------------------------------------------------------------*/
-/*--- Callgrind                                                    ---*/
-/*---                                                     global.h ---*/
-/*--- (C) 2004, 2005 Josef Weidendorfer                            ---*/
+/*--- Callgrind data structures, functions.               global.h ---*/
 /*--------------------------------------------------------------------*/
+
+/*
+   This file is part of Valgrind, a dynamic binary instrumentation
+   framework.
+
+   Copyright (C) 2004-2014 Josef Weidendorfer
+      josef.weidendorfer@gmx.de
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307, USA.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
 
 #ifndef CLG_GLOBAL
 #define CLG_GLOBAL
@@ -28,7 +51,7 @@
 
 
 /*------------------------------------------------------------*/
-/*--- Calltree compile options                            --- */
+/*--- Callgrind compile options                           --- */
 /*------------------------------------------------------------*/
 
 /* Enable debug output */

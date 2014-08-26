e12b5e5ac2e944fb1ffea4f9e5ce39e008a8a864
Mark Wielaard <mark@klomp.org>
2014-08-26 22:15:58 +0000

    Fix typo in syswrap ppoll param, equate ppoll and poll for memcheck testcase.
    ppoll argument is called ufds like the poll argument.
    Newer architectures (aarch64) implement poll by calling ppoll directly.
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@14370

--- coregrind/m_syswrap/syswrap-linux.c
+++ coregrind/m_syswrap/syswrap-linux.c
@@ -1271,7 +1271,7 @@ PRE(sys_ppoll)
                     (Addr)(&ufds[i].fd), sizeof(ufds[i].fd) );
       PRE_MEM_READ( "ppoll(ufds.events)",
                     (Addr)(&ufds[i].events), sizeof(ufds[i].events) );
-      PRE_MEM_WRITE( "ppoll(ufd.revents)",
+      PRE_MEM_WRITE( "ppoll(ufds.revents)",
                      (Addr)(&ufds[i].revents), sizeof(ufds[i].revents) );
    }
 

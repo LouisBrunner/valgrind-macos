5633e22712d28a609175810a7baa382d90c82809
Mark Wielaard <mark@klomp.org>
2014-09-10 12:08:15 +0000

    Bug 338974 glibc 2.20 changed size of struct sigaction sa_flags on s390
    To track POSIX glibc and the kernel agreed to change the type of the
    sa_flags field in struct sigaction to an int. There is now a int
    __glibc_reserved0 padding field that can be passed undefined.
    See https://sourceware.org/ml/libc-alpha/2014-09/msg00161.html
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@14508

--- include/vki/vki-s390x-linux.h
+++ include/vki/vki-s390x-linux.h
@@ -258,7 +258,13 @@ struct vki_old_sigaction {
 struct vki_sigaction {
         // [[See comment about extra 'k' above]]
         __vki_sighandler_t ksa_handler;
-        unsigned long sa_flags;
+        // Yes, the reserved field is really glibc specific. The kernel
+        // doesn't have it and uses an unsigned long for sa_flags.
+        // The glibc and the kernel agreed this is fine and the
+        // __glibc_reserved0 field can be undefined.
+        // See https://sourceware.org/ml/libc-alpha/2014-09/msg00161.html
+        int __glibc_reserved0;
+        int sa_flags;
         void (*sa_restorer)(void);
         vki_sigset_t sa_mask;               /* mask last for extensibility */
 };

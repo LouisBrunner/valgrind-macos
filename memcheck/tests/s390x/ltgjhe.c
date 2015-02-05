c95c10bea0811ab2f7820d6cdfe44e0202d2197e
Christian Borntraeger <borntraeger@de.ibm.com>
2015-02-05 11:05:53 +0000

    valgrind part to VEX:3083 Fix bugzilla 343802, also extend testcase
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@14905

--- memcheck/tests/s390x/ltgjhe.c
+++ memcheck/tests/s390x/ltgjhe.c
@@ -17,7 +17,7 @@ int main()
 			"jhe 1f\n\t"
 			"lghi %0,0\n\t"
 			"j 2f\n\t"
-			"1:lghi %0,0\n\t"
+			"1:lghi %0,1\n\t"
 			"2:\n\t"
 			:"=d" (result)
 			:"Q" (field1)
@@ -31,6 +31,36 @@ int main()
 			LT(0,0,1,000,00)
 			"jhe 1f\n\t"
 			"lghi %0,0\n\t"
+			"j 2f\n\t"
+			"1:lghi %0,1\n\t"
+			"2:\n\t"
+			:"=d" (result)
+			:"Q" (field2)
+			:"0","cc");
+
+	if (result)
+		printf("Error\n");
+
+	asm volatile(	"oi %1,128\n\t"
+			"la 1,%1\n\t"
+			LTG(0,0,1,000,00)
+			"jl 1f\n\t"
+			"lghi %0,1\n\t"
+			"j 2f\n\t"
+			"1:lghi %0,0\n\t"
+			"2:\n\t"
+			:"=d" (result)
+			:"Q" (field1)
+			:"0","cc");
+
+	if (result)
+		printf("Error\n");
+
+	asm volatile(	"oi %1,128\n\t"
+			"la 1,%1\n\t"
+			LT(0,0,1,000,00)
+			"jl 1f\n\t"
+			"lghi %0,1\n\t"
 			"j 2f\n\t"
 			"1:lghi %0,0\n\t"
 			"2:\n\t"

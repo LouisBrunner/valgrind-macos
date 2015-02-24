12401cdc4118485645ff5a0d103771de25e626ee
Florian Krohm <florian@eich-krohm.de>
2015-02-24 18:11:01 +0000

    Add a comment explaining why there are only 3 emulation warnings and not 4 as one would expect. Update the .exp file to show correct line numbers.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@14962

--- none/tests/s390x/fpext_warn.c
+++ none/tests/s390x/fpext_warn.c
@@ -11,6 +11,10 @@ main(void)
    __asm__ volatile ( CEGBRA(1,0,0,0) : : : "cc", "memory");
    __asm__ volatile ( CEFBRA(3,0,0,0) : : : "cc", "memory");
    __asm__ volatile ( CDGBRA(4,0,0,0) : : : "cc", "memory");
+
+   /* Note: an emulation warning is expected for the following
+      insn but none is given. The reason is that at most 3 warnings
+      of a given kind will be issued - and we already had three. */
    __asm__ volatile ( CEFBRA(5,0,0,0) : : : "cc", "memory");
 
    printf("after\n");

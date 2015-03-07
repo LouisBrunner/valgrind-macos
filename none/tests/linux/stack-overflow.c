b77e045e1fd3e699ce4c0e4969ef1f19e6da35ae
Florian Krohm <florian@eich-krohm.de>
2015-03-07 12:49:32 +0000

    Rewritten to use a single line instead of line splicing to avoid line number differences in the reported error.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@14988

--- none/tests/linux/stack-overflow.c
+++ none/tests/linux/stack-overflow.c
@@ -1,9 +1,5 @@
 /* There should be a user message about the overflow.
-   Wrtten in a single line so there is no confusion on what line
+   Written in a single line so there is no confusion on what line
    the overflow occurs. */
 
-int main(int argc, char *argv[]) \
-{ \
-  volatile int arr[1000]; \
-  return main(arr[argc%2], 0);                    \
-}
+int main(int argc, char *argv[]) { volatile int arr[1000]; return main(arr[argc%2], 0); }

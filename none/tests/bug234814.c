09b00c49c496a3e667e29fd0a29a1ac9ea31be91
Rhys Kidd <rhyskidd@gmail.com>
2015-05-23 10:20:06 +0000

    Add (presently) failing test case for bz#234814.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15282

---
+++ none/tests/bug234814.c
@@ -0,0 +1,30 @@
+/* Refer https://bugs.kde.org/show_bug.cgi?id=234814
+ */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+const char kSigbus[] = "I caught the SIGBUS signal!\n";
+
+int GLOB = 3;
+
+void mysigbus() {
+   write(2, kSigbus, sizeof(kSigbus));
+   GLOB--;
+   return;
+}
+
+int main() {
+   struct sigaction sa;
+   sa.sa_handler = mysigbus;
+   sigemptyset(&sa.sa_mask);
+   sa.sa_flags = 0;
+   if (sigaction(SIGBUS, &sa, NULL) == -1) {
+      perror("ERROR:");
+   }
+   while(GLOB) {
+      kill(getpid(), SIGBUS);
+   };
+   return 0;
+} 

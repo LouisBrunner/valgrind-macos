3adbf40a38b52d3f329de9a4c1b548d1364b176a
Bart Van Assche <bvanassche@acm.org>
2015-06-28 16:52:49 +0000

    Follow-up for r15381
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15382

---
+++ include/vki/vki-xen-schedop.h
@@ -0,0 +1,18 @@
+#ifndef __VKI_XEN_SCHED_OP_H
+#define __VKI_XEN_SCHED_OP_H
+
+#define VKI_XEN_SCHEDOP_yield           0
+
+#define VKI_XEN_SCHEDOP_block           1
+
+#define VKI_XEN_SCHEDOP_shutdown        2
+
+#define VKI_XEN_SCHEDOP_poll            3
+
+#define VKI_XEN_SCHEDOP_remote_shutdown 4
+
+#define VKI_XEN_SCHEDOP_shutdown_code   5
+
+#define VKI_XEN_SCHEDOP_watchdog        6
+
+#endif /* __VKI_XEN_SCHED_OP_H */

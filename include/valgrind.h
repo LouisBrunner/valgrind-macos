0a8d1755ec1ecebb05a4305e7116e797bbf1ae1c
Julian Seward <jseward@acm.org>
2015-09-23 13:35:36 +0000

    Merge, from branches/VALGRIND_3_11_BRANCH, NEWS and docs/xml/vg-entities.xml.
    Bump version to 3.12.0.SVN.
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15679

--- include/valgrind.h
+++ include/valgrind.h
@@ -89,7 +89,7 @@
         || (__VALGRIND_MAJOR__ == 3 && __VALGRIND_MINOR__ >= 6))
 */
 #define __VALGRIND_MAJOR__    3
-#define __VALGRIND_MINOR__    10
+#define __VALGRIND_MINOR__    12
 
 
 #include <stdarg.h>

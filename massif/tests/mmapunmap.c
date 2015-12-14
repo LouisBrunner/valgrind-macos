683c52fb7ed4ab1e73b049f5d7b2de8e8a6b88bc
Ivo Raisr <ivosh@ivosh.net>
2015-12-14 20:14:44 +0000

    Fix expected output of massif/tests/mmapunmap on Solaris.
    On Solaris and Linux, mmap() is an optimized function without
    prologue and epilogue. However, Solaris libc does not currently
    bear any DWARF CFI. Therefore stack chain unwinder grabs only
    two entries: IP inside mmap() and return address from the previous
    frame which points inside _start(), right after where main()  
    is invoked. By introducing an intermediate function f(), main()
    is now visible in the stack trace even on Solaris.
    n-i-bz
    
    
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15749

--- massif/tests/mmapunmap.c
+++ massif/tests/mmapunmap.c
@@ -4,13 +4,16 @@
 #include <unistd.h>
 #include "tests/sys_mman.h"
 
+static void *f(void)
+{
+   return mmap(NULL, 80 * 1000 * 1024,
+               PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS,
+               -1, 0);
+}
+
 int main()
 {
-   void *m;
-
-   m = mmap(NULL, 80 * 1000 * 1024,
-            PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS,
-            -1, 0);
+   void *m = f();
    munmap(m, 80 * 1000 * 1024);
    return 0;
 }

22a45562037356642e74a6ae51c427bba5b34b0b
Ivo Raisr <ivosh@ivosh.net>
2016-07-11 21:10:47 +0000

    Ignore pselect_sigmask_null executable. Part of fix for BZ#364413.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15904

--- none/tests/pselect_sigmask_null.c
+++ none/tests/pselect_sigmask_null.c
@@ -1,5 +1,5 @@
 /* Make sure handling of NULL sigmask is correct.
-   https://bugs.kde.org/show_bug.cgi?id=XXX
+   https://bugs.kde.org/show_bug.cgi?id=364413
    We might try to make a copy and adjust the mask.
    Testcase provided by Paul Eggert <eggert@cs.ucla.edu> */
 

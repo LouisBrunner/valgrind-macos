49a0fdf408d8c131db7573e77634c4488dc1e48a
Julian Seward <jseward@acm.org>
2016-07-24 11:40:07 +0000

    Enable PCMPxSTRx cases 0x62 and 0x72, and reformat the associated switch statements a bit more consistently.  Fixes #353384 and #353727.
    git-svn-id: svn://svn.valgrind.org/vex/trunk@3230

--- VEX/priv/guest_generic_x87.c
+++ VEX/priv/guest_generic_x87.c
@@ -795,11 +795,16 @@ Bool compute_PCMPxSTRx ( /*OUT*/V128* resV,
       even if they would probably work.  Life is too short to have
       unvalidated cases in the code base. */
    switch (imm8) {
-      case 0x00: case 0x02: case 0x08: case 0x0A: case 0x0C: case 0x0E:
-      case 0x12: case 0x14: case 0x18: case 0x1A:
-      case 0x30: case 0x34: case 0x38: case 0x3A:
-      case 0x40: case 0x42: case 0x44: case 0x46: case 0x4A:
-      case 0x70:
+      case 0x00: case 0x02:
+      case 0x08: case 0x0A: case 0x0C: case 0x0E:
+                 case 0x12: case 0x14:
+      case 0x18: case 0x1A:
+      case 0x30:            case 0x34:
+      case 0x38: case 0x3A:
+      case 0x40: case 0x42: case 0x44: case 0x46:
+                 case 0x4A:
+                 case 0x62:
+      case 0x70: case 0x72:
          break;
       default:
          return False;

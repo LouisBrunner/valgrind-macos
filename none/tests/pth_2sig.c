b25f61d6ffa5ddaac96fc040d1e824ac07a7e54a
Ivo Raisr <ivosh@ivosh.net>
2016-11-20 15:06:08 +0000

    Fix test none/tests/pth_2sig on Solaris. When signal SIGINT was used, it got propagated to the parent shell and therefore the regression test suite was terminated. With SIGTERM only the parent exits, as is supposed. Verified that this changed test case still reproduces the original problem on Linux and Solaris, when r16140 is temporarily undone. Follow up to r16140 and r16143 and BZ#372600.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@16147

--- none/tests/pth_2sig.c
+++ none/tests/pth_2sig.c
@@ -1,45 +1,41 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
 #include <pthread.h>
-#include <assert.h>
+#include <signal.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
-void *
-slavethread(void *arg)
+
+void *slavethread(void *arg)
 {
-   char c;
-   while (1)
-      (void)read(0, &c, 1);
+    while (1)
+        pause();
 }
 
 int main(int argc, char **argv)
 {
-    pthread_t slave;
-    int i = 0;
-    int pid = getpid();
+    for (int i = 0; i < 10; i++) {
+        pthread_t slave;
+        if (pthread_create(&slave, 0, slavethread, 0)) {
+            perror("pthread_create");
+            exit(2);
+        }
+    }
 
-    for (i = 0; i < 10; i++)
-       if (pthread_create(&slave, 0, &slavethread, 0))
-          {
-             perror("slave2");
-             exit(2);
-          }
-    switch (fork())
-       {
-       case 0: // child
-          sleep(2); // Should be enough to ensure (some) threads are created
-          for (i = 0; i < 20 && kill(pid, 2) == 0; i++)
-             ;
-          exit(0);
-       case -1:
-          perror("fork");
-          exit(4);
-       }
+    pid_t pid = getpid();
+    switch (fork()) {
+        case 0: // child
+            sleep(2); // Should be enough to ensure (some) threads are created
+            for (int i = 0; i < 20 && kill(pid, SIGTERM) == 0; i++)
+                ;
+            exit(0);
+        case -1:
+            perror("fork");
+            exit(4);
+    }
 
-    while (rand() >= 0)
-       i++;
-    fprintf(stderr, "strange, this program is supposed to be killed !!!!\n");
+    while (1)
+        pause(); 
+    fprintf(stderr, "strange, this program is supposed to be killed!\n");
     return 1;
 }

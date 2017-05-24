d8837cc0fa244f1379584310108f7f48abd42f31
Julian Seward <jseward@acm.org>
2017-05-24 14:07:49 +0000

    Enable fair scheduling by default on Linux.  n-i-bz.
    git-svn-id: svn://svn.valgrind.org/valgrind/trunk@16414

--- coregrind/m_options.c
+++ coregrind/m_options.c
@@ -105,8 +105,13 @@ Bool   VG_(clo_debug_dump_syms) = False;
 Bool   VG_(clo_debug_dump_line) = False;
 Bool   VG_(clo_debug_dump_frames) = False;
 Bool   VG_(clo_trace_redir)    = False;
-enum FairSchedType
-       VG_(clo_fair_sched)     = disable_fair_sched;
+
+#if defined(VGO_linux)
+enum FairSchedType VG_(clo_fair_sched) = enable_fair_sched;
+#else
+enum FairSchedType VG_(clo_fair_sched) = disable_fair_sched;
+#endif
+
 Bool   VG_(clo_trace_sched)    = False;
 Bool   VG_(clo_profile_heap)   = False;
 Int    VG_(clo_core_redzone_size) = CORE_REDZONE_DEFAULT_SZB;

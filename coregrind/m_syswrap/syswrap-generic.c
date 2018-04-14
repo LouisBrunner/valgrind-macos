LouisBrunner/valgrind-macos

commit 54145019b045fffde625447b64f3a91f663de718

n-i-bz  Fix possible stack trashing by semctl syscall wrapping
The modified test none/tests/sem crashes with a SEGV when valgrind is compiled
with lto on various amd64 platforms (debian/gcc 6.3, RHEL7/gcc 6.4,
Ubuntu/gcc 7.2)

The problem is that the vki_semid_ds buf is not what is expected by the kernel:
the kernel expects a bigger structure vki_semid64_ds (at least on
these platforms).
Getting the sem_nsems seems to work by chance, as sem_nsems is at
the same offset in both vki_semid_ds and vki_semid64_ds.
However, e.g. the ctime was not set properly after syscall return,
and 2 words after sem_nsems were set to 0 by the kernel, causing
the SEGV, as a spilled register became 0.

Fix consists in using the 64 bit version for __NR_semctl.
Tested on debian/amd64 and s390x.

coregrind/m_syswrap/syswrap-generic.c
@@ -1790,30 +1790,36 @@ ML_(generic_PRE_sys_semtimedop) ( ThreadId tid,
 static
 UInt get_sem_count( Int semid )
 {
-   struct vki_semid_ds buf;
    union vki_semun arg;
    SysRes res;
 
-   /* Doesn't actually seem to be necessary, but gcc-4.4.0 20081017
-      (experimental) otherwise complains that the use in the return
-      statement below is uninitialised. */
-   buf.sem_nsems = 0;
-
-   arg.buf = &buf;
-
 #  if defined(__NR_semctl)
+   struct vki_semid64_ds buf;
+   arg.buf64 = &buf;
    res = VG_(do_syscall4)(__NR_semctl, semid, 0, VKI_IPC_STAT, *(UWord *)&arg);
-#  elif defined(__NR_semsys) /* Solaris */
-   res = VG_(do_syscall5)(__NR_semsys, VKI_SEMCTL, semid, 0, VKI_IPC_STAT,
-                          *(UWord *)&arg);
-#  else
-   res = VG_(do_syscall5)(__NR_ipc, 3 /* IPCOP_semctl */, semid, 0,
-                          VKI_IPC_STAT, (UWord)&arg);
-#  endif
    if (sr_isError(res))
       return 0;
 
    return buf.sem_nsems;
+#  elif defined(__NR_semsys) /* Solaris */
+   struct vki_semid_ds buf;
+   arg.buf = &buf;
+   res = VG_(do_syscall5)(__NR_semsys, VKI_SEMCTL, semid, 0, VKI_IPC_STAT,
+                          *(UWord *)&arg);
+   if (sr_isError(res))
+      return 0;
+
+   return buf.sem_nsems;
+#  else
+   struct vki_semid_ds buf;
+   arg.buf = &buf;
+   res = VG_(do_syscall5)(__NR_ipc, 3 /* IPCOP_semctl */, semid, 0,
+                          VKI_IPC_STAT, (UWord)&arg);
+   if (sr_isError(res))
+      return 0;
+
+   return buf.sem_nsems;
+#  endif
 }
 
 void

include/vki/vki-linux.h
@@ -1205,6 +1205,7 @@ struct vki_sembuf {
 union vki_semun {
 	int val;			/* value for SETVAL */
 	struct vki_semid_ds __user *buf;	/* buffer for IPC_STAT & IPC_SET */
+	struct vki_semid64_ds __user *buf64;	/* buffer for IPC_STAT & IPC_SET */
 	unsigned short __user *array;	/* array for GETALL & SETALL */
 	struct vki_seminfo __user *__buf;	/* buffer for IPC_INFO */
 	void __user *__pad;

none/tests/sem.c
@@ -8,6 +8,59 @@
 #include <sys/sem.h>
 #include <time.h>
 #include <unistd.h>
+
+void semctl_test (int trace, const char *fname)
+{
+  key_t key;
+  int semid;
+  int nr_of_readers;
+  int ret;
+
+  union semun {
+     int              val;    /* Value for SETVAL */
+     struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+     unsigned short  *array;  /* Array for GETALL, SETALL */
+     struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                                 (Linux-specific) */
+  } u;
+
+  struct semid_ds ds;
+
+  key = ftok (fname, 1);
+  if (key == -1)
+    perror ("ftok");
+  nr_of_readers = 4;
+
+  semid = semget (key, 2 * nr_of_readers, IPC_CREAT + 0660);
+  if (semid == -1) {
+    perror ("semget");
+  }
+  if (trace)
+     printf("semid %d\n", semid);
+
+  u.buf = &ds;
+  ret = semctl (semid, 0, IPC_STAT, u);
+  if (ret == -1)
+     perror("semctl IPC_STAT");
+  if (trace)
+     printf("semid %d sem_nsems %d\n", semid, (int) ds.sem_nsems);
+
+  {
+    unsigned short semarray[2 * nr_of_readers];
+    for (int count = 0; count < nr_of_readers; count++) {
+        semarray[2 * count] = 0;
+        semarray[2 * count + 1] = 1000;
+      }
+    ret = semctl (semid, 0, SETALL, semarray);
+    if (ret == -1)
+      perror ("semctl SETALL");
+  }
+
+  ret = semctl (semid, 0, IPC_RMID);
+  if (ret == -1)
+    perror ("semctl IPC_RMID");
+}
+
 int main(int argc, char **argv)
 {
    int semid;
@@ -98,6 +151,7 @@ int main(int argc, char **argv)
       perror("semctl(IPC_RMID)");
       exit(1);
    }
-         
+
+   semctl_test(argc > 1, argv[0]);
    exit(0);
 }

commit 06cb991bcd9966b614cd672a7d6e26785f60f851

Unbreak build on macos,
(problem introduced by 54145019b045fffde625447b64f3a91f663de718)

coregrind/m_syswrap/syswrap-generic.c
@@ -1794,13 +1794,20 @@ UInt get_sem_count( Int semid )
    SysRes res;
 
 #  if defined(__NR_semctl)
+#  if defined(VGO_darwin)
+   /* Darwin has no specific 64 bit semid_ds, but has __NR_semctl. */
+   struct vki_semid_ds buf;
+   arg.buf = &buf;
+#  else
    struct vki_semid64_ds buf;
    arg.buf64 = &buf;
+#  endif
    res = VG_(do_syscall4)(__NR_semctl, semid, 0, VKI_IPC_STAT, *(UWord *)&arg);
    if (sr_isError(res))
       return 0;
 
    return buf.sem_nsems;
+
 #  elif defined(__NR_semsys) /* Solaris */
    struct vki_semid_ds buf;
    arg.buf = &buf;
@@ -1810,6 +1817,7 @@ UInt get_sem_count( Int semid )
       return 0;
 
    return buf.sem_nsems;
+
 #  else
    struct vki_semid_ds buf;
    arg.buf = &buf;
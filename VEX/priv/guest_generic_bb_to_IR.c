dd627dc127a6767c497f027de6eab634815f7861
Andreas Arnez <arnez@linux.ibm.com>
2020-02-11 18:02:38 +0100

    s390x: Activate "grail"
    Now that the known problems with activating "grail" on s390x have been
    fixed, there is no need to disable it for s390x guests any more.  Remove
    the appropriate check in "guest_generic_bb_to_IR.c".

--- VEX/priv/guest_generic_bb_to_IR.c
+++ VEX/priv/guest_generic_bb_to_IR.c
@@ -1449,11 +1449,6 @@ IRSB* bb_to_IR (
       // hope of identifying and recovering, an "A && B" condition spread across
       // two basic blocks.
       if (irsb_be.tag == Be_Cond
-          /* sewardj 2019Nov30: Alas, chasing cond branches on s390 causes
-             Memcheck to crash, for as-yet unknown reasons.  It also exposes
-             some unhandled Iex_ITE cases in the s390x instruction selector.
-             For now, disable. */
-          && arch_guest != VexArchS390X
           /* sewardj 2019Dec14: It also causes crashing on MIPS, even for
              --tool=none. */
           && arch_guest != VexArchMIPS64 && arch_guest != VexArchMIPS32)

cc9cb5d42ee978f29373744552ae04e8909ac0dd
Tom Hughes <tom@compton.nu>
2020-02-20 08:43:58 +0000

    Synchronise CLONE_xxx flags with kernel source

--- include/vki/vki-linux.h
+++ include/vki/vki-linux.h
@@ -380,20 +380,31 @@ struct vki_utimbuf {
 // From linux-2.6.8.1/include/linux/sched.h
 //----------------------------------------------------------------------
 
-#define VKI_CSIGNAL		0x000000ff	/* signal mask to be sent at exit */
-#define VKI_CLONE_VM		0x00000100	/* set if VM shared between processes */
-#define VKI_CLONE_FS		0x00000200	/* set if fs info shared between processes */
-#define VKI_CLONE_FILES		0x00000400	/* set if open files shared between processes */
-#define VKI_CLONE_SIGHAND	0x00000800	/* set if signal handlers and blocked signals shared */
-#define VKI_CLONE_VFORK		0x00004000	/* set if the parent wants the child to wake it up on mm_release */
-#define VKI_CLONE_PARENT	0x00008000	/* set if we want to have the same parent as the cloner */
-#define VKI_CLONE_THREAD	0x00010000	/* Same thread group? */
-#define VKI_CLONE_SYSVSEM	0x00040000	/* share system V SEM_UNDO semantics */
-#define VKI_CLONE_SETTLS	0x00080000	/* create a new TLS for the child */
-#define VKI_CLONE_PARENT_SETTID	0x00100000	/* set the TID in the parent */
+#define VKI_CSIGNAL			0x000000ff	/* signal mask to be sent at exit */
+#define VKI_CLONE_VM			0x00000100	/* set if VM shared between processes */
+#define VKI_CLONE_FS			0x00000200	/* set if fs info shared between processes */
+#define VKI_CLONE_FILES			0x00000400	/* set if open files shared between processes */
+#define VKI_CLONE_SIGHAND		0x00000800	/* set if signal handlers and blocked signals shared */
+#define VKI_CLONE_PIDFD			0x00001000	/* set if a pidfd should be placed in parent */
+#define VKI_CLONE_PTRACE		0x00002000	/* set if we want to let tracing continue on the child too */
+#define VKI_CLONE_VFORK			0x00004000	/* set if the parent wants the child to wake it up on mm_release */
+#define VKI_CLONE_PARENT		0x00008000	/* set if we want to have the same parent as the cloner */
+#define VKI_CLONE_THREAD		0x00010000	/* Same thread group? */
+#define VKI_CLONE_NEWNS			0x00020000	/* New mount namespace group */
+#define VKI_CLONE_SYSVSEM		0x00040000	/* share system V SEM_UNDO semantics */
+#define VKI_CLONE_SETTLS		0x00080000	/* create a new TLS for the child */
+#define VKI_CLONE_PARENT_SETTID		0x00100000	/* set the TID in the parent */
 #define VKI_CLONE_CHILD_CLEARTID	0x00200000	/* clear the TID in the child */
-#define VKI_CLONE_DETACHED	0x00400000	/* Unused, ignored */
-#define VKI_CLONE_CHILD_SETTID	0x01000000	/* set the TID in the child */
+#define VKI_CLONE_DETACHED		0x00400000	/* Unused, ignored */
+#define VKI_CLONE_UNTRACED		0x00800000	/* set if the tracing process can't force CLONE_PTRACE on this clone */
+#define VKI_CLONE_CHILD_SETTID		0x01000000	/* set the TID in the child */
+#define VKI_CLONE_NEWCGROUP		0x02000000	/* New cgroup namespace */
+#define VKI_CLONE_NEWUTS		0x04000000	/* New utsname namespace */
+#define VKI_CLONE_NEWIPC		0x08000000	/* New ipc namespace */
+#define VKI_CLONE_NEWUSER		0x10000000	/* New user namespace */
+#define VKI_CLONE_NEWPID		0x20000000	/* New pid namespace */
+#define VKI_CLONE_NEWNET		0x40000000	/* New network namespace */
+#define VKI_CLONE_IO			0x80000000	/* Clone io context */
 
 struct vki_sched_param {
 	int sched_priority;

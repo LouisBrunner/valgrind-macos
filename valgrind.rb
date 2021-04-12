c9338d20467a87467953a6c94dd6e014d42a09a9
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2021-04-12 14:15:28 +0200

    Use new Ruby 1.9 hash syntax for sha256 and depends_on

--- valgrind.rb
+++ valgrind.rb
@@ -7,12 +7,12 @@ class Valgrind < Formula
     mirror "https://dl.bintray.com/homebrew/mirror/valgrind-3.14.0.tar.bz2"
     sha256 "037c11bfefd477cc6e9ebe8f193bb237fe397f7ce791b4a4ce3fa1c6a520baa5"
 
-    depends_on :maximum_macos => :high_sierra
+    depends_on maximum_macos: :high_sierra
   end
 
   bottle do
-    sha256 "7869473ca1009d871dfcb496cc4d08e0318315d18721854ef42960b76e2ef64d" => :high_sierra
-    sha256 "5ac984d472025c7bbc081e3be88b31f709944cf924945ebe85427f00d7cca73e" => :sierra
+    sha256 high_sierra: "7869473ca1009d871dfcb496cc4d08e0318315d18721854ef42960b76e2ef64d"
+    sha256 sierra:      "5ac984d472025c7bbc081e3be88b31f709944cf924945ebe85427f00d7cca73e"
   end
 
   head do

b128b20e66adbf00e87cf43e00e07ffe04f2ba8b
Paul Floyd <pjfloyd@wanadoo.fr>
2021-10-06 22:58:15 +0200

    FreeBSD support, patch 7
    tests directory
    configure.ac to generate tests/filter_discards and tests/filter_stderr_basic
    .gitignore to ignore these generated filters

--- tests/malloc.h
+++ tests/malloc.h
@@ -1,8 +1,12 @@
 // Replacement for malloc.h which factors out platform differences.
 
 #include <stdlib.h>
+#include "config.h"
 #if defined(VGO_darwin)
 #  include <malloc/malloc.h>
+#elif defined(VGO_freebsd)
+#  include <stdlib.h>
+#  include <malloc_np.h>
 #else
 #  include <malloc.h>
 #endif
@@ -14,7 +18,7 @@ __attribute__((unused))
 static void* memalign16(size_t szB)
 {
    void* x;
-#if defined(VGO_darwin)
+#if defined(VGO_darwin) || defined(VGO_freebsd)
    // Darwin lacks memalign, but its malloc is always 16-aligned anyway.
    posix_memalign((void **)&x, 16, szB);
 #else
@@ -30,7 +34,7 @@ __attribute__((unused))
 static void* memalign32(size_t szB)
 {
    void* x;
-#if defined(VGO_darwin)
+#if defined(VGO_darwin) || defined(VGO_freebsd)
    // Darwin lacks memalign
    posix_memalign((void **)&x, 32, szB);
 #else
@@ -46,7 +50,7 @@ __attribute__((unused))
 static void* memalign64(size_t szB)
 {
    void* x;
-#if defined(VGO_darwin)
+#if defined(VGO_darwin) || defined(VGO_freebsd)
    // Darwin lacks memalign
    posix_memalign((void **)&x, 64, szB);
 #else

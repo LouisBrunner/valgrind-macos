7ada63a4487ed6ebf10dbae809f992065f11299e
Louis Brunner <LouisBrunner@users.noreply.github.com>
2023-01-04 10:22:55 +0000

    fix: mkdirat was not properly added (#66)

--- coregrind/m_syswrap/syswrap-darwin.c
+++ coregrind/m_syswrap/syswrap-darwin.c
@@ -11572,6 +11572,7 @@ const SyscallTableEntry ML_(syscall_table)[] = {
    MACX_(__NR_faccessat,           faccessat),          // 466
    MACXY(__NR_fstatat64,           fstatat64),          // 470
    MACX_(__NR_readlinkat,          readlinkat),         // 473
+   MACX_(__NR_mkdirat,             mkdirat),            // 475
    MACX_(__NR_bsdthread_ctl,       bsdthread_ctl),      // 478
    MACXY(__NR_csrctl,              csrctl),             // 483
    MACX_(__NR_guarded_open_dprotected_np, guarded_open_dprotected_np),  // 484

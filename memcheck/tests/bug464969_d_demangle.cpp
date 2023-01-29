82282a6676a5c1b3784fd22fc097a0076dfcb7ca
Paul Floyd <pjfloyd@wanadoo.fr>
2023-01-29 22:05:27 +0100

    Bug 464969 -  D language demangling
        Patch from witold.baryluk@gmail.com
    
    (also added a testcase)

---
+++ memcheck/tests/bug464969_d_demangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+extern "C" void _D4test4findFiPxaZPxa()
+{
+   int a;
+   if (a)
+      std::cout << "a\n";
+   else
+      std::cout << "!a\n";
+}
+
+int main()
+{
+   _D4test4findFiPxaZPxa();
+}
+
+

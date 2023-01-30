42d35e6d2ad7e83414e82a691956777064106624
Mark Wielaard <mark@klomp.org>
2023-01-30 10:21:01 +0100

    bug464969_d_demangle.cpp: Always print "Xa"
    bug464969_d_demangle depends on producing an error about an
    uninitialized variable. But it prints something different
    to stdout based on the value of that variable. Which can
    produce failures. Make sure to always print the same thing
    to stdout.

--- memcheck/tests/bug464969_d_demangle.cpp
+++ memcheck/tests/bug464969_d_demangle.cpp
@@ -4,9 +4,9 @@ extern "C" void _D4test4findFiPxaZPxa()
 {
    int a;
    if (a)
-      std::cout << "a\n";
+      std::cout << "Xa\n";
    else
-      std::cout << "!a\n";
+      std::cout << "X" << "a\n";
 }
 
 int main()

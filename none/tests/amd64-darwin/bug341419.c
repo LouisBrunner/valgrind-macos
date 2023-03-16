7c71c0fd6742261aa76f01baa18e6f0bd45b9a70
Louis Brunner <louis.brunner.fr@gmail.com>
2023-03-16 15:07:01 +0000

    r11

--- none/tests/amd64-darwin/bug341419.c
+++ none/tests/amd64-darwin/bug341419.c
@@ -64,8 +64,8 @@ void this_function_halts(unsigned long long a0, unsigned long long a1,
     __asm__ volatile("movq $0xfeed01010101cafe,%rax");
     __asm__ volatile("movq $0xfeed02020202cafe,%rbx");
     __asm__ volatile("movq $0xfeed03030303cafe,%r10");
-#if DARWIN_VERS < DARWIN_12_00
     __asm__ volatile("movq $0xfeed04040404cafe,%r11");
+#if DARWIN_VERS < DARWIN_12_00
     __asm__ volatile("movq $0xfeed05050505cafe,%r12");
     __asm__ volatile("movq $0xfeed06060606cafe,%r13");
     __asm__ volatile("movq $0xfeed07070707cafe,%r14");
@@ -88,8 +88,8 @@ void this_function_int3s(unsigned long long a0, unsigned long long a1,
     __asm__ volatile("movq $0xfeed01010101cafe,%rax");
     __asm__ volatile("movq $0xfeed02020202cafe,%rbx");
     __asm__ volatile("movq $0xfeed03030303cafe,%r10");
-#if DARWIN_VERS < DARWIN_12_00
     __asm__ volatile("movq $0xfeed04040404cafe,%r11");
+#if DARWIN_VERS < DARWIN_12_00
     __asm__ volatile("movq $0xfeed05050505cafe,%r12");
     __asm__ volatile("movq $0xfeed06060606cafe,%r13");
     __asm__ volatile("movq $0xfeed07070707cafe,%r14");
@@ -158,8 +158,8 @@ handle_signal(int sig, siginfo_t *si, void *vuc)
 
     ASSERT_EQ(uc->uc_mcontext->__ss.__rbx, 0xfeed02020202cafe);
     ASSERT_EQ(uc->uc_mcontext->__ss.__r10, 0xfeed03030303cafe);
-#if DARWIN_VERS < DARWIN_12_00
     ASSERT_EQ(uc->uc_mcontext->__ss.__r11, 0xfeed04040404cafe);
+#if DARWIN_VERS < DARWIN_12_00
     ASSERT_EQ(uc->uc_mcontext->__ss.__r12, 0xfeed05050505cafe);
     ASSERT_EQ(uc->uc_mcontext->__ss.__r13, 0xfeed06060606cafe);
 // Starting with macOS 12.0, dyld uses r14 for its own purposes,

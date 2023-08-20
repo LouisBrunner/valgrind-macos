3ee80eb4584424498147215e31603e2a2223d315
Paul Floyd <pjfloyd@wanadoo.fr>
2023-08-20 09:04:17 +0200

    FreeBSD: try to make eventfd2 testcase deterministic
    Add a sleep to give child 1 a chance to run.
    Flush stdout every time.

--- memcheck/tests/freebsd/eventfd2.c
+++ memcheck/tests/freebsd/eventfd2.c
@@ -18,6 +18,7 @@ static void xsem_wait(int fd)
 
    fprintf(stdout, "wait completed on %d: count=%" PRIu64 "\n",
            fd, cntr);
+   fflush(stdout);
 }
 
 static void xsem_post(int fd, int count)
@@ -38,21 +39,27 @@ static void sem_player(int fd1, int fd2)
     * (also xsem_wait above)
     */
    fprintf(stdout, "posting 1 on %d\n", fd1);
+   fflush(stdout);
    xsem_post(fd1, 1);
 
    fprintf(stdout, "waiting on %d\n", fd2);
+   fflush(stdout);
    xsem_wait(fd2);
 
    fprintf(stdout, "posting 1 on %d\n", fd1);
+   fflush(stdout);
    xsem_post(fd1, 1);
 
    fprintf(stdout, "waiting on %d\n", fd2);
+   fflush(stdout);
    xsem_wait(fd2);
 
    fprintf(stdout, "posting 5 on %d\n", fd1);
+   fflush(stdout);
    xsem_post(fd1, 5);
 
    fprintf(stdout, "waiting 5 times on %d\n", fd2);
+   fflush(stdout);
    xsem_wait(fd2);
    xsem_wait(fd2);
    xsem_wait(fd2);
@@ -88,6 +95,7 @@ int main(int argc, char **argv)
       sem_player(fd1, fd2);
       exit(0);
    }
+   sleep(1);
    if ((cpid_waiter = fork()) == 0) {
       sem_player(fd2, fd1);
       exit(0);

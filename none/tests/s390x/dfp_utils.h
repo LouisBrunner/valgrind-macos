673b497030e3f2281a62e841a8b7187eaf176d64
Andreas Arnez <arnez@linux.ibm.com>
2023-09-07 16:43:24 +0200

    Bug 472875 - s390x regtest: Rework dfp-1
    The dfp-1 test case has two main issues:
    
    * It doesn't initialize the condition code before executing the DFP
      instructions for "multiply" and "divide", but asserts a certain value
      afterwards.  (See Bug 472875.)
    
    * It doesn't compile with Clang.  (See Bug 465782.)
    
    Fix these with a rework.  The first issue can be fixed by initializing the
    condition code before executing the DFP instructions.  The second issue
    requires avoiding any `_Decimal*' types, since they are not supported by
    Clang.

--- none/tests/s390x/dfp_utils.h
+++ none/tests/s390x/dfp_utils.h
@@ -4,22 +4,19 @@
 #include <stddef.h>      /* size_t */
 #include <stdio.h>       /* printf */
 
+typedef float reg_d32;
+typedef double reg_d64;
+typedef long double reg_d128;
+typedef union { unsigned int i; reg_d32 f; } pun_d32;
+typedef union { unsigned long i; reg_d64 f; } pun_d64;
+typedef union { unsigned long i[2]; reg_d128 f; } pun_d128;
+
 #define DFP_VAL_PRINT(op, type)                                         \
   {                                                                     \
     enum { n = sizeof(type) };                                          \
     union { type x; unsigned char i[n]; } u = { .x = op };              \
     for (int k = 0; k < n; k++)                                         \
        printf("%02x", u.i[k]);                                          \
-  }
-
-#define DFP_BINOP_PRINT(op1, op2, result, type, op, cc)                 \
-  {                                                                     \
-    DFP_VAL_PRINT(op1, type);                                           \
-    printf(" "op" ");                                                   \
-    DFP_VAL_PRINT(op2, type);                                           \
-    printf(" = ");                                                      \
-    DFP_VAL_PRINT(result, type);                                        \
-    printf(" cc = %d\n", cc);                                           \
   }
 
 #endif /* DFP_UTILS_H */

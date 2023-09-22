LouisBrunner/valgrind-macos

commit 60f7e89ba32b54d73b9e36d49e28d0f559ade0b9

Support lazy reading and downloading of DWARF debuginfo
Currently valgrind attempts to read DWARF .debug_* sections as well
as separate debuginfo files for ELF binaries as soon as a shared library
is loaded.  This might also result in the downloading of separate debuginfo
files via debuginfod.

This is inefficient when some of this debuginfo never ends up being used
by valgrind while running the client process.

This patch adds support for lazy reading and downloading of DWARF
debuginfo.  When an ELF shared library is loaded, the reading of .debug_*
sections as well as separate or alternate debuginfo is deferred until
valgrind handles an instruction pointer corresponding to a text segment
of the shared library.  At this point the deferred sections and separate
debug files are loaded.

This feature is only supported on ELF platforms.

https://bugs.kde.org/show_bug.cgi?id=471807

ChangeLog
	* debuginfo.c (di_notify_ACHIEVE_ACCEPT_STATE): Replace
	read_elf_debug_info with read_elf_object.
	(addr_load_di): New function.  Attempts to load deferred debuginfo
	associated with a given address.
	(load_di): New function.  Attempts to load a given deferred
	debuginfo associated with a given address.
	(describe_IP): Add calls to load_di and addr_load_di.
	(find_DiCfSI): Add call to load_di.

	* priv_readelf.h (read_elf_object): New declaration.
	(read_elf_debug): Ditto.

	* priv_storage.h (struct _DebugInfo): New field 'bool deferred'.

	* readelf.c (read_elf_debug_info): Split into read_elf_object and
	read_elf_debug.
	(read_elf_object): Read non .debug_* section from an ELF binary.
	(read_elf_debug): Read .debug_* sections from an ELF binary as
	as well any separate/alternate debuginfo files.

	* storage.c (canonicaliseSymtab): Remove assert in order to support
	canonicalization of deferred _DebugInfo.
	(finish_CFSI_arrays): Add early return if _DebugInfo is
	deferred in order to avoid freeing memory that will be needed
	when reading debuginfo at a later time.

coregrind/m_debuginfo/priv_storage.h
@@ -678,6 +678,13 @@ struct _DebugInfo {
       invalid and should not be consulted. */
    Bool  have_dinfo; /* initially False */
 
+   /* If true then the reading of .debug_* section has been deferred
+      until it this information is required (such as when printing
+      a stacktrace).  Additionally, if true then the reading of any
+      separate debuginfo files associated with this object has also
+      been deferred. */
+   Bool deferred;
+
    /* All the rest of the fields in this structure are filled in once
       we have committed to reading the symbols and debug info (that
       is, at the point where .have_dinfo is set to True). */

coregrind/m_debuginfo/storage.c
@@ -1297,8 +1297,7 @@ void ML_(addVar)( struct _DebugInfo* di,
       that those extra sections have the same bias as .text, but that
       seems a reasonable assumption to me. */
    /* This is assured us by top level steering logic in debuginfo.c,
-      and it is re-checked at the start of
-      ML_(read_elf_debug_info). */
+      and it is re-checked at the start of ML_(read_elf_object). */
    vg_assert(di->fsm.have_rx_map && di->fsm.rw_map_count);
    if (level > 0 && ML_(find_rx_mapping)(di, aMin, aMax) == NULL) {
       if (VG_(clo_verbosity) > 1) {
@@ -1725,7 +1724,6 @@ static void canonicaliseSymtab ( struct _DebugInfo* di )
    for (i = 0; i < di->symtab_used; i++) {
       DiSym* sym = &di->symtab[i];
       vg_assert(sym->pri_name);
-      vg_assert(!sym->sec_names);
    }
 
    /* Sort by address. */
@@ -2383,6 +2381,9 @@ void ML_(finish_CFSI_arrays) ( struct _DebugInfo* di )
    vg_assert (f_holes == n_holes);
    vg_assert (pos == new_used);
 
+   if (di->deferred)
+      return;
+
    di->cfsi_used = new_used;
    di->cfsi_size = new_used;
    ML_(dinfo_free) (di->cfsi_rd);
@@ -2398,9 +2399,13 @@ void ML_(canonicaliseTables) ( struct _DebugInfo* di )
    canonicaliseLoctab ( di );
    canonicaliseInltab ( di );
    ML_(canonicaliseCFI) ( di );
+   canonicaliseVarInfo ( di );
+
+   if (di->deferred)
+      return;
+
    if (di->cfsi_m_pool)
       VG_(freezeDedupPA) (di->cfsi_m_pool, ML_(dinfo_shrink_block));
-   canonicaliseVarInfo ( di );
    if (di->strpool)
       VG_(freezeDedupPA) (di->strpool, ML_(dinfo_shrink_block));
    if (di->fndnpool)

coregrind/m_debuginfo/debuginfo.c
@@ -959,14 +959,16 @@ static ULong di_notify_ACHIEVE_ACCEPT_STATE ( struct _DebugInfo* di )
    discard_DebugInfos_which_overlap_with( di );
 
    /* The DebugInfoMappings that now exist in the FSM may involve
-      overlaps.  This confuses ML_(read_elf_debug_info), and may cause
+      overlaps.  This confuses ML_(read_elf_*), and may cause
       it to compute wrong biases.  So de-overlap them now.
       See http://bugzilla.mozilla.org/show_bug.cgi?id=788974 */
    truncate_DebugInfoMapping_overlaps( di, di->fsm.maps );
 
    /* And acquire new info. */
 #  if defined(VGO_linux) || defined(VGO_solaris) || defined(VGO_freebsd)
-   ok = ML_(read_elf_debug_info)( di );
+   ok = ML_(read_elf_object)( di );
+   if (ok)
+      di->deferred = True;
 #  elif defined(VGO_darwin)
    ok = ML_(read_macho_debug_info)( di );
 #  else
@@ -1443,6 +1445,50 @@ ULong VG_(di_notify_mmap)( Addr a, Bool allow_SkFileV, Int use_fd )
    }
 }
 
+/* Load DI if it has a text segment containing A and DI hasn't already
+   been loaded.  */
+
+void VG_(load_di)( DebugInfo *di, Addr a)
+{
+   if (!di->deferred
+       || !di->text_present
+       || di->text_size <= 0
+       || di->text_avma > a
+       || a >= di->text_avma + di->text_size)
+      return;
+
+   di->deferred = False;
+   ML_(read_elf_debug) (di);
+   ML_(canonicaliseTables)( di );
+
+   /* Check invariants listed in
+      Comment_on_IMPORTANT_REPRESENTATIONAL_INVARIANTS in
+      priv_storage.h. */
+   check_CFSI_related_invariants(di);
+   ML_(finish_CFSI_arrays)(di);
+}
+
+/* Attempt to load DebugInfo with a text segment containing A,
+   if such a debuginfo hasn't already been loaded.  */
+
+void VG_(addr_load_di)( Addr a )
+{
+   DebugInfo *di;
+
+   di = VG_(find_DebugInfo)(VG_(current_DiEpoch)(), a);
+   if (di != NULL)
+      if (di->deferred) {
+         di->deferred = False;
+         ML_(read_elf_debug) (di);
+         ML_(canonicaliseTables)( di );
+
+         /* Check invariants listed in
+            Comment_on_IMPORTANT_REPRESENTATIONAL_INVARIANTS in
+            priv_storage.h. */
+         check_CFSI_related_invariants(di);
+         ML_(finish_CFSI_arrays)(di);
+      }
+}
 
 /* Unmap is simpler - throw away any SegInfos intersecting 
    [a, a+len).  */
@@ -2746,6 +2792,11 @@ const HChar* VG_(describe_IP)(DiEpoch ep, Addr eip, const InlIPCursor *iipc)
    Bool  know_objname;
    Bool  know_srcloc;
 
+   if (iipc && iipc->di)
+      VG_(load_di) (iipc->di, eip);
+   else
+      VG_(addr_load_di) (eip);
+
    if (is_bottom(iipc)) {
       // At the bottom (towards main), we describe the fn at eip.
       know_fnname = VG_(clo_sym_offsets)
@@ -3089,6 +3140,8 @@ static void find_DiCfSI ( /*OUT*/DebugInfo** diP,
 
       if (!is_DI_valid_for_epoch(di, curr_epoch))
          continue;
+
+      VG_(load_di)(di, ip);
 
       /* Use the per-DebugInfo summary address ranges to skip
          inapplicable DebugInfos quickly. */

coregrind/m_debuginfo/priv_readelf.h
@@ -44,13 +44,23 @@
 extern Bool ML_(is_elf_object_file)( const void* image, SizeT n_image,
                                      Bool rel_ok );
 
-/* The central function for reading ELF debug info.  For the
-   object/exe specified by the SegInfo, find ELF sections, then read
-   the symbols, line number info, file name info, CFA (stack-unwind
-   info) and anything else we want, into the tables within the
-   supplied SegInfo.
-*/
-extern Bool ML_(read_elf_debug_info) ( DebugInfo* di );
+/* Read the ELF binary specified by DI.  For the object/exe specified
+   by the SegInfo, find ELF sections, then read the symbols, line number
+   info, file name info, CFA (stack-unwind info) and anything else we
+   want, into the tables within the supplied SegInfo.
+
+   .debug_* sections as well as any separate debuginfo files are not
+   loaded by this function but instead by ML_(read_elf_debug).  This
+   separation facilitates lazy loading of debuginfo.  */
+extern Bool ML_(read_elf_object) ( DebugInfo* di );
+
+/* Read .debug_* sections from the ELF binary specified by DI.  Also
+   attempt to load any separate debuginfo files associated with the
+   object.
+
+   ML_(read_elf_object) should be called on DI before calling this
+   function.  */
+extern Bool ML_(read_elf_debug) ( DebugInfo* di );
 
 extern Bool ML_(check_elf_and_get_rw_loads) ( Int fd, const HChar* filename, Int * rw_load_count );
 

coregrind/pub_core_debuginfo.h
@@ -76,6 +76,10 @@ extern void VG_(di_notify_pdb_debuginfo)( Int fd, Addr avma,
 extern void VG_(di_notify_vm_protect)( Addr a, SizeT len, UInt prot );
 #endif
 
+extern void VG_(addr_load_di)( Addr a );
+
+extern void VG_(load_di)( DebugInfo *di, Addr a );
+
 extern void VG_(di_discard_ALL_debuginfo)( void );
 
 /* Like VG_(get_fnname), but it does not do C++ demangling nor Z-demangling

commit eb0b07f4fc63fec057f1b59cce8cb308110a4552

debuginfo.c: Add more hooks for lazy loading debuginfo
Add calls to di_load_di in search_one_{symtab,loctab,cfitab} to ensure
that debuginfo is always loaded before searching these tables.

This fixes a bug introduced in commit 60f7e89ba32 that results in
callgrind failing to read source filenames from debuginfo.

Also add a test which checks that callgrind successfully finds a
filename from debuginfo.

Co-Authored-By: Philippe Waroquiers <philippe.waroquiers@skynet.be>

coregrind/m_debuginfo/priv_storage.h
@@ -1162,16 +1162,16 @@ extern void ML_(finish_CFSI_arrays) ( struct _DebugInfo* di );
 
 /* Find a symbol-table index containing the specified pointer, or -1
    if not found.  Binary search.  */
-extern Word ML_(search_one_symtab) ( const DebugInfo* di, Addr ptr,
+extern Word ML_(search_one_symtab) ( DebugInfo* di, Addr ptr,
                                      Bool findText );
 
 /* Find a location-table index containing the specified pointer, or -1
    if not found.  Binary search.  */
-extern Word ML_(search_one_loctab) ( const DebugInfo* di, Addr ptr );
+extern Word ML_(search_one_loctab) ( DebugInfo* di, Addr ptr );
 
 /* Find a CFI-table index containing the specified pointer, or -1 if
    not found.  Binary search.  */
-extern Word ML_(search_one_cfitab) ( const DebugInfo* di, Addr ptr );
+extern Word ML_(search_one_cfitab) ( DebugInfo* di, Addr ptr );
 
 /* Find a FPO-table index containing the specified pointer, or -1
    if not found.  Binary search.  */

coregrind/m_debuginfo/storage.c
@@ -2420,9 +2420,10 @@ void ML_(canonicaliseTables) ( struct _DebugInfo* di )
 /* Find a symbol-table index containing the specified pointer, or -1
    if not found.  Binary search.  */
 
-Word ML_(search_one_symtab) ( const DebugInfo* di, Addr ptr,
+Word ML_(search_one_symtab) ( DebugInfo* di, Addr ptr,
                               Bool findText )
 {
+   VG_(di_load_di)(di);
    Addr a_mid_lo, a_mid_hi;
    Word mid,
         lo = 0, 
@@ -2449,8 +2450,9 @@ Word ML_(search_one_symtab) ( const DebugInfo* di, Addr ptr,
 /* Find a location-table index containing the specified pointer, or -1
    if not found.  Binary search.  */
 
-Word ML_(search_one_loctab) ( const DebugInfo* di, Addr ptr )
+Word ML_(search_one_loctab) ( DebugInfo* di, Addr ptr )
 {
+   VG_(di_load_di)(di);
    Addr a_mid_lo, a_mid_hi;
    Word mid, 
         lo = 0, 
@@ -2473,8 +2475,9 @@ Word ML_(search_one_loctab) ( const DebugInfo* di, Addr ptr )
 /* Find a CFI-table index containing the specified pointer, or -1
    if not found.  Binary search.  */
 
-Word ML_(search_one_cfitab) ( const DebugInfo* di, Addr ptr )
+Word ML_(search_one_cfitab) ( DebugInfo* di, Addr ptr )
 {
+   VG_(di_load_di)(di);
    Word mid, 
         lo = 0, 
         hi = di->cfsi_used-1;

callgrind/tests/Makefile.am
@@ -21,7 +21,8 @@ EXTRA_DIST = \
 	notpower2-hwpref.vgtest notpower2-hwpref.stderr.exp \
 	notpower2-use.vgtest notpower2-use.stderr.exp \
 	threads.vgtest threads.stderr.exp \
-	threads-use.vgtest threads-use.stderr.exp
+	threads-use.vgtest threads-use.stderr.exp \
+	find-source.vgtest find-source.stderr.exp find-source.post.exp
 
 check_PROGRAMS = clreq simwork threads
 

callgrind/tests/find-source.stderr.exp
@@ -1,0 +1,6 @@
+
+
+Events    : Ir
+Collected :
+
+I   refs:

callgrind/tests/find-source.vgtest
@@ -1,0 +1,6 @@
+# The 'prog' doesn't matter because we don't use its output. Instead we test
+# the post-processing of the cgout-test file.
+prog: ../../tests/true
+vgopts: --callgrind-out-file=callgrind.out
+post: grep -q 'tests/true\.c' callgrind.out
+cleanup: rm callgrind.out
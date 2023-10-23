a2f3d97c84a65b0ff0ba562c5f82afd1aaa082ec
Louis Brunner <LouisBrunner@users.noreply.github.com>
2023-10-24 01:03:07 +0200

    feat: add macOS 14 support (#98)

--- coregrind/m_mach/mach_msg.c
+++ coregrind/m_mach/mach_msg.c
@@ -191,6 +191,16 @@ mach_msg_trap(mach_msg_header_t *msg,
               mach_msg_timeout_t timeout,
               mach_port_t notify);
 
+mach_msg_return_t mach_msg(
+  mach_msg_header_t *msg,
+  mach_msg_option_t option,
+  mach_msg_size_t send_size,
+  mach_msg_size_t rcv_size,
+  mach_port_t rcv_name,
+  mach_msg_timeout_t timeout,
+  mach_port_t notify
+);
+
 mach_msg_return_t
 mach_msg(msg, option, send_size, rcv_size, rcv_name, timeout, notify)
     mach_msg_header_t *msg;
--- coregrind/m_syswrap/syswrap-amd64-darwin.c
+++ coregrind/m_syswrap/syswrap-amd64-darwin.c
@@ -489,7 +489,8 @@ void wqthread_hijack(Addr self, Addr kport, Addr stackaddr, Addr workitem,
             || DARWIN_VERS == DARWIN_10_15 \
             || DARWIN_VERS == DARWIN_11_00 \
             || DARWIN_VERS == DARWIN_12_00 \
-            || DARWIN_VERS == DARWIN_13_00
+            || DARWIN_VERS == DARWIN_13_00 \
+            || DARWIN_VERS == DARWIN_14_00
        UWord magic_delta = 0xE0;
 #      else
 #        error "magic_delta: to be computed on new OS version"

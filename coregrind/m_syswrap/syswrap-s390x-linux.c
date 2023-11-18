ca88e1c63178186a1f6a0d0b1a4ba4ee895af323
Mark Wielaard <mark@klomp.org>
2023-11-18 02:23:10 +0100

    s390x-linux also has memfd_secret, syswrap it

--- coregrind/m_syswrap/syswrap-s390x-linux.c
+++ coregrind/m_syswrap/syswrap-s390x-linux.c
@@ -874,6 +874,8 @@ static SyscallTableEntry syscall_table[] = {
    LINX_(__NR_faccessat2,  sys_faccessat2),                           // 439
 
    LINXY(__NR_epoll_pwait2, sys_epoll_pwait2),                        // 441
+
+   LINXY(__NR_memfd_secret, sys_memfd_secret),                        // 447
 };
 
 SyscallTableEntry* ML_(get_linux_syscall_entry) ( UInt sysno )

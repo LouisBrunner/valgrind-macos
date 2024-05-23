8b374fd2bce60a58809b4f95a93d7e2f01929a08
Alexandra Hájková <ahajkova@redhat.com>
2024-05-23 04:46:26 -0400

    Improve none/tests/double_close_range test and filter_fdleak
    Adjust none/tests/file_dclose, none/tests/fdleak_ipv4
    and socket_close to fit the changes in filter_fdleak.
    
    https://bugs.kde.org/show_bug.cgi?id=488441

--- none/tests/double_close_range.c
+++ none/tests/double_close_range.c
@@ -9,6 +9,8 @@
 
 int main ()
 {
+   // Create a new file descriptor that we immediately close.
+   // This is of course fine.
    int fd = dup (2);
    if (fd != -1){
       fprintf(stderr, "close fd %d\n", fd);
@@ -16,11 +18,18 @@ int main ()
    }
    
    // Shouldn't warn, we are closing everything
+   // This is a special case for close_range if the last
+   // argument is ~0U (infinity).
    fprintf(stderr, "Closing range (%d, %d).\n", fd, ~0U);
-   close_range(6, ~0U, 0);
+   close_range(fd, ~0U, 0);
 
-   // Should warn, we close a small range (but only for fd itself
-   fprintf(stderr, "Closing range (%d, %d).\n", fd, 7);
+   int fd5 = dup2(1, 5);
+   int fd7 = dup2(2, 7);
+   fprintf(stderr, "Closing range (%d, %d).\n", fd5, fd7);
+   close_range(5, 7, 0);
+
+   // Should warn, we close a small range
+   fprintf(stderr, "Double closing range (%d, %d).\n", fd5, fd7);
    close_range(5, 7, 0);
 
    return 0;

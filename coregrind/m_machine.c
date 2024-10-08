338afebb7e804c236a63ab4f433301a72d78dcde
Louis Brunner <louis.brunner.fr@gmail.com>
2024-10-08 05:19:19 +0100

    fix ci for macos14

--- coregrind/m_machine.c
+++ coregrind/m_machine.c
@@ -1871,26 +1871,36 @@ Bool VG_(machine_get_hwcaps)( void )
 #define IS_ENABLED(mib, miblen) \
         (VG_(sysctl)(mib, (miblen), &val, &len, NULL, 0) == 0 && val == 1)
 
-     // sysctlbyname("hw.optional.arm.FEAT_PAuth")
-     Int mibPAUTH[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_PAUTH};
-     if (IS_ENABLED(mibPAUTH, 4)) {
+     Int mibCheck[] = {VKI_CTL_HW,VKI_HW_NCPU};
+     if (!IS_ENABLED(mibCheck, 2)) {
+        // some virtual machines are missing most values in sysctlbyname so we fallback to good defaults
         vai.hwcaps |= VEX_HWCAPS_ARM64_PAUTH;
-     }
-     // sysctlbyname("hw.optional.arm.FEAT_LRCPC")
-     Int mibLRCPC[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_LRCPC};
-     if (IS_ENABLED(mibLRCPC, 4)) {
         vai.hwcaps |= VEX_HWCAPS_ARM64_LRCPC;
-     }
-     // sysctlbyname("hw.optional.arm.FEAT_DIT")
-     Int mibDIT[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_DIT};
-     if (IS_ENABLED(mibDIT, 4)) {
-         vai.hwcaps |= VEX_HWCAPS_ARM64_DIT;
-     }
-     // sysctlbyname("hw.optional.arm.FEAT_FP16")
-      Int mibFP16[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_FP16};
-     if (IS_ENABLED(mibFP16, 4)) {
-         vai.hwcaps |= VEX_HWCAPS_ARM64_FP16;
-         vai.hwcaps |= VEX_HWCAPS_ARM64_VFP16; // FIXME: is that true?
+        vai.hwcaps |= VEX_HWCAPS_ARM64_DIT;
+        vai.hwcaps |= VEX_HWCAPS_ARM64_FP16;
+        vai.hwcaps |= VEX_HWCAPS_ARM64_VFP16;
+     } else {
+        // sysctlbyname("hw.optional.arm.FEAT_PAuth")
+        Int mibPAUTH[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_PAUTH};
+        if (IS_ENABLED(mibPAUTH, 4)) {
+            vai.hwcaps |= VEX_HWCAPS_ARM64_PAUTH;
+        }
+        // sysctlbyname("hw.optional.arm.FEAT_LRCPC")
+        Int mibLRCPC[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_LRCPC};
+        if (IS_ENABLED(mibLRCPC, 4)) {
+            vai.hwcaps |= VEX_HWCAPS_ARM64_LRCPC;
+        }
+        // sysctlbyname("hw.optional.arm.FEAT_DIT")
+        Int mibDIT[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_DIT};
+        if (IS_ENABLED(mibDIT, 4)) {
+            vai.hwcaps |= VEX_HWCAPS_ARM64_DIT;
+        }
+        // sysctlbyname("hw.optional.arm.FEAT_FP16")
+          Int mibFP16[] = {VKI_CTL_HW,VKI_HW_OPTIONAL,VKI_HW_ARM,VKI_HW_FEAT_FP16};
+        if (IS_ENABLED(mibFP16, 4)) {
+            vai.hwcaps |= VEX_HWCAPS_ARM64_FP16;
+            vai.hwcaps |= VEX_HWCAPS_ARM64_VFP16; // FIXME: is that true?
+        }
      }
 
 #undef IS_ENABLED

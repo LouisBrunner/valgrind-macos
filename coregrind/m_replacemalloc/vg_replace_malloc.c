f29bc61a22097c4e5ca966ad874647ef596f0327
Paul Floyd <pjfloyd@wanadoo.fr>
2024-10-28 20:36:17 +0100

    Bug 495469 - aligned_alloc and posix_memalign missing MALLOC_TRACE with returned pointer

--- coregrind/m_replacemalloc/vg_replace_malloc.c
+++ coregrind/m_replacemalloc/vg_replace_malloc.c
@@ -1781,6 +1781,7 @@ extern int * __error(void) __attribute__((weak));
       MALLOC_TRACE("reallocarray(%p,%llu,%llu)", ptrV, (ULong)nmemb, (ULong)size ); \
       if (nmemb > 0 && (SizeT)-1 / nmemb < size) { \
          SET_ERRNO_ENOMEM; \
+         MALLOC_TRACE(" = 0\n"); \
          return NULL; \
       } \
       v = (void*)VALGRIND_NON_SIMD_CALL2( info.tl_realloc, ptrV, nmemb*size ); \
@@ -1987,6 +1988,7 @@ extern int * __error(void) __attribute__((weak));
            (alignment & (alignment - 1)) != 0) ||                              \
           (VG_MEMALIGN_ALIGN_FACTOR_FOUR && (alignment % 4 != 0))) {           \
          SET_ERRNO_EINVAL;                                                     \
+         MALLOC_TRACE(" = 0\n");                                               \
          return 0;                                                             \
       }                                                                        \
       /* Round up to minimum alignment if necessary. */                        \
@@ -2005,6 +2007,8 @@ extern int * __error(void) __attribute__((weak));
                                                                                \
       if (!mem)                                                                \
          SET_ERRNO_ENOMEM;                                                     \
+                                                                               \
+      MALLOC_TRACE(" = %p\n", mem);                                            \
                                                                                \
       return mem;                                                              \
    }
@@ -2048,6 +2052,8 @@ extern int * __error(void) __attribute__((weak));
                pszB, pszB, size ); \
       \
       if (!mem) SET_ERRNO_ENOMEM; \
+      \
+      MALLOC_TRACE(" = %p\n", mem); \
       \
       return mem; \
    }
@@ -2188,12 +2194,14 @@ extern int * __error(void) __attribute__((weak));
       if (alignment == 0 \
           || alignment % sizeof (void *) != 0 \
           || (alignment & (alignment - 1)) != 0) { \
+         MALLOC_TRACE(" = 0\n"); \
          return VKI_EINVAL; \
       } \
       if (VG_POSIX_MEMALIGN_SIZE_0_RETURN_NULL && \
           size == 0U) { \
          /* no allocation for zero size on Solaris/Illumos */ \
          *memptr = NULL; \
+         MALLOC_TRACE(" = 0\n"); \
          return 0; \
       } \
       /* Round up to minimum alignment if necessary. */ \
@@ -2202,6 +2210,8 @@ extern int * __error(void) __attribute__((weak));
       \
       mem = (void*)VALGRIND_NON_SIMD_CALL3( info.tl_memalign, \
                alignment, orig_alignment, size ); \
+      \
+      MALLOC_TRACE(" = %p\n", mem); \
       \
       if (mem != NULL) { \
         *memptr = mem; \
@@ -2328,6 +2338,8 @@ extern int * __error(void) __attribute__((weak));
        mem = (void*)VALGRIND_NON_SIMD_CALL3( info.tl_memalign, \
                  alignment, orig_alignment, size ); \
        \
+       MALLOC_TRACE(" = %p\n", mem); \
+       \
        return mem; \
     }
 
@@ -2353,6 +2365,7 @@ extern int * __error(void) __attribute__((weak));
            || (VG_ALIGNED_ALLOC_ALIGN_POWER_TWO && (alignment & (alignment - 1)) != 0) \
            || (VG_ALIGNED_ALLOC_ALIGN_FACTOR_FOUR && (alignment % 4 != 0))) { \
           SET_ERRNO_EINVAL; \
+          MALLOC_TRACE(" = 0\n"); \
           return 0; \
        } \
        \
@@ -2366,6 +2379,8 @@ extern int * __error(void) __attribute__((weak));
                  alignment, orig_alignment, size ); \
        \
        if (!mem) SET_ERRNO_ENOMEM; \
+       \
+       MALLOC_TRACE(" = %p\n", mem); \
        \
        return mem; \
     }

fd1608c55b5f1d466536e635ce6805a022a2f8c0
Mark Wielaard <mark@klomp.org>
2025-01-23 22:59:42 +0100

    memcheck/tests/amd64/rh2257546_256.c: use xmm instead of ymm in clobbers
    Older gcc (4.8.5) doesn't recognize yym registers as clobbers. Use xmm
    instead, as in the AVX configure checks, which are the same register
    (half) just with a different name.

--- memcheck/tests/amd64/rh2257546_256.c
+++ memcheck/tests/amd64/rh2257546_256.c
@@ -28,7 +28,7 @@ int main ( void )
    "zzz1f:"                            "\n\t"
    "mov $88, %0"                       "\n"
    "zzzafter:"                         "\n\t"
-   : /*OUT*/"=r"(res) : /*IN*/"r"(c1),"r"(c2) : /*TRASH*/"ymm4","ymm5","cc"
+   : /*OUT*/"=r"(res) : /*IN*/"r"(c1),"r"(c2) : /*TRASH*/"xmm4","xmm5","cc"
    );
    printf("res = %lld\n", res);
    free(c1);

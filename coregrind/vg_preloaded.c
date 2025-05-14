2fa8581c4974db57de80de915d16ce299eb9aa84
Khem Raj <raj.khem@gmail.com>
2025-05-14 09:29:33 -0700

    Use portable syntax for pushsection directive in inline assembly
    '@' does not work with clang inline assembler, but '%' works with both
    gcc and clang. Therefore use '%' to make it more portable
    
    Fixes
    <inline asm>:1:41: error: expected '%<type>' or "<type>"
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- coregrind/vg_preloaded.c
+++ coregrind/vg_preloaded.c
@@ -55,7 +55,7 @@
 /* Note: The "MS" section flags are to remove duplicates.  */
 #define DEFINE_GDB_PY_SCRIPT(script_name) \
   asm("\
-.pushsection \".debug_gdb_scripts\", \"MS\",@progbits,1\n\
+.pushsection \".debug_gdb_scripts\", \"MS\",%progbits,1\n\
 .byte 1 /* Python */\n\
 .asciz \"" script_name "\"\n\
 .popsection \n\

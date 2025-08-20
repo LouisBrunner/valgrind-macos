LouisBrunner/valgrind-macos

commit e3f078d8470ce8c08e97c4a34092616896dfcd9f

s390: Support Iop_ExpCmpNE8 and Iop_ExpCmpNE16.
Easy as pie and fewer special cases for iropt-test.

VEX/priv/host_s390_isel.c
@@ -3638,12 +3638,14 @@ s390_isel_cc(ISelEnv *env, IRExpr *cond)
 
       case Iop_CmpNE8:
       case Iop_CasCmpNE8:
+      case Iop_ExpCmpNE8:
          op     = S390_ZERO_EXTEND_8;
          result = S390_CC_NE;
          goto do_compare_ze;
 
       case Iop_CmpEQ16:
       case Iop_CasCmpEQ16:
+      case Iop_ExpCmpNE16:
          op     = S390_ZERO_EXTEND_16;
          result = S390_CC_E;
          goto do_compare_ze;

commit 957d7350cf28cc3ff177c13e45ed23c51cb53f10

s390: Fix handling of Iop_ExpCmpNE16
Bug was introduced by yours truly in e3f078d847.

VEX/priv/host_s390_isel.c
@@ -3658,13 +3658,13 @@ s390_isel_cc(ISelEnv *env, IRExpr *cond)
 
       case Iop_CmpEQ16:
       case Iop_CasCmpEQ16:
-      case Iop_ExpCmpNE16:
          op     = S390_ZERO_EXTEND_16;
          result = S390_CC_E;
          goto do_compare_ze;
 
       case Iop_CmpNE16:
       case Iop_CasCmpNE16:
+      case Iop_ExpCmpNE16:
          op     = S390_ZERO_EXTEND_16;
          result = S390_CC_NE;
          goto do_compare_ze;
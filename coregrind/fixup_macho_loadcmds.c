7137bf8a32b5d1a01a0ec5786309420390baf539
Louis Brunner <louis.brunner.fr@gmail.com>
2025-10-12 16:24:24 +0100

    chore: fix expectations for Linux + macOS 15 x86_64

--- coregrind/fixup_macho_loadcmds.c
+++ coregrind/fixup_macho_loadcmds.c
@@ -126,7 +126,8 @@
     && DARWIN_VERS != DARWIN_10_13 && DARWIN_VERS != DARWIN_10_14 \
     && DARWIN_VERS != DARWIN_10_15 && DARWIN_VERS != DARWIN_11_00 \
     && DARWIN_VERS != DARWIN_12_00 && DARWIN_VERS != DARWIN_13_00 \
-    && DARWIN_VERS != DARWIN_14_00 && DARWIN_VERS != DARWIN_15_00
+    && DARWIN_VERS != DARWIN_14_00 && DARWIN_VERS != DARWIN_15_00 \
+    && DARWIN_VERS != DARWIN_15_04
 #  error "Unknown DARWIN_VERS value.  This file only compiles on Darwin."
 #endif
 

b559dd2c07ceec4686c0bc16e0c30bbee8d070e2
Louis Brunner <louis.brunner.fr@gmail.com>
2026-06-17 00:11:06 +0100

    more tweaks

--- .github/sysctl_tester.c
+++ .github/sysctl_tester.c
@@ -13,13 +13,15 @@ static void test_sysctl(const char *name) {
 }
 
 int main() {
+  test_sysctl("hw.cputype");
+  test_sysctl("hw.cpusubtype");
   test_sysctl("hw.ncpu");
-  test_sysctl("hw.optional.arm.FEAT_PAuth");
-  test_sysctl("hw.optional.arm.FEAT_LRCPC");
-  test_sysctl("hw.optional.arm.FEAT_DIT");
-  test_sysctl("hw.optional.arm.FEAT_FP16");
   test_sysctl("hw.optional.arm.FEAT_SHA3");
   test_sysctl("hw.optional.arm.FEAT_SB");
+  test_sysctl("hw.optional.arm.FEAT_LRCPC");
   test_sysctl("hw.optional.arm.FEAT_JSCVT");
+  test_sysctl("hw.optional.arm.FEAT_PAuth");
+  test_sysctl("hw.optional.arm.FEAT_DIT");
+  test_sysctl("hw.optional.arm.FEAT_FP16");
   return 0;
 }
